823c40cbeeab79ac792c9a32b69915fff8b3adad
Andrew Lima <andrew@arctek.co.za>
2022-01-13 15:20:27 +0200

    Linting stuff.
    Just linting our files now. Doing this separately.

--- frontend-pages/filter-pmpro-levels-page-levels.php
+++ frontend-pages/filter-pmpro-levels-page-levels.php
@@ -1,22 +1,22 @@
 <?php
 /**
- * Filter what level options may be selected based on the current member's active level. 
+ * Filter what level options may be selected based on the current member's active level.
  * This example allows you to show/hide specific levels on the Membership Levels page.
- * 
+ *
  * title: Dynamically Display Certain Levels Based on the Current User’s Active Level
  * layout: snippet
  * collection: frontend-pages
  * category: levels, level-page
- * 
+ *
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
  * Read this companion article for step-by-step directions on either method.
  * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
  */
-function dynamic_pmpro_levels_array( $levels ) {	
-	// Get all the levels
+function dynamic_pmpro_levels_array( $levels ) {
+	// Get all the levels.
 	$levels = pmpro_getAllLevels( false, true );
-	
+
 	// remove levels 1, 5, 6, and 7 user has level 1.
 	if ( pmpro_hasMembershipLevel( '1' ) ) {
 		unset( $levels['1'] );
@@ -32,13 +32,13 @@ function dynamic_pmpro_levels_array( $levels ) {
 		unset( $levels['4'] );
 	}
 	// only show levels 1 and 2 if user has no level.
-	if ( ! pmpro_hasMembershipLevel( ) ) {
+	if ( ! pmpro_hasMembershipLevel() ) {
 		unset( $levels['3'] );
 		unset( $levels['4'] );
 		unset( $levels['5'] );
 		unset( $levels['6'] );
 		unset( $levels['7'] );
-	}	
+	}
 	return $levels;
 }
 add_filter( 'pmpro_levels_array', 'dynamic_pmpro_levels_array', 10, 2 );
--- frontend-pages/hide-levels-from-levels-page-by-id.php
+++ frontend-pages/hide-levels-from-levels-page-by-id.php
@@ -1,26 +1,26 @@
 <?php
 /**
  * Hide levels from the level select page based off specific level ID's.
- * 
+ *
  * title: Hiding Specific Levels (by ID) from the Membership Levels Display
  * layout: snippet
  * collection: frontend-pages
  * category: levels, level-page
- * 
+ *
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
  * Read this companion article for step-by-step directions on either method.
  * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
  */
 function my_pmpro_levels_array( $levels ) {
-	//a comma-separated list of the levels to hide
+	// a comma-separated list of the levels to hide.
 	$hiddenlevels = array( 1, 3 );
 
-	//build the filtered levels array
+	// build the filtered levels array.
 	$newlevels = array();
-	foreach( $levels as $key => $level ) {
+	foreach ( $levels as $key => $level ) {
 		if ( ! in_array( $level->id, $hiddenlevels ) ) {
-			$newlevels[$key] = $level;
+			$newlevels[ $key ] = $level;
 		}
 	}
 	return $newlevels;
