cccb62a67a204954234f1371193174c35a20802f
Andrew Lima <andrew@arctek.co.za>
2022-01-13 12:13:53 +0200

    Hide levels based on ID
    Hide levels on the level select page based on level ID.
    
    Reference: https://www.paidmembershipspro.com/memberships-levels-page-order-hide-display-skip-mega-post/

---
+++ frontend-pages/hide-levels-from-levels-page-by-id.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Hide levels from the level select page based off specific level ID's.
+ * 
+ * title: Hiding Specific Levels (by ID) from the Membership Levels Display
+ * layout: snippet
+ * collection: frontend-pages
+ * category: levels, level-page
+ * 
+ * You can add this recipe to your site by creating a custom plugin
+ * or using the Code Snippets plugin available for free in the WordPress repository.
+ * Read this companion article for step-by-step directions on either method.
+ * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
+ */
+function my_pmpro_levels_array( $levels ) {
+	//a comma-separated list of the levels to hide
+	$hiddenlevels = array( 1, 3 );
+
+	//build the filtered levels array
+	$newlevels = array();
+	foreach( $levels as $key => $level ) {
+		if ( ! in_array( $level->id, $hiddenlevels ) ) {
+			$newlevels[$key] = $level;
+		}
+	}
+	return $newlevels;
+}
+add_filter( 'pmpro_levels_array', 'my_pmpro_levels_array' );
