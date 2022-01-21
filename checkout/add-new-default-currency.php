ff2e365c1c12585b1ae3e4bb6e1743894472bb40
Jarryd Long <JarrydLong@users.noreply.github.com>
2022-01-21 10:15:55 +0200

    New Snippets
    Reference: https://www.paidmembershipspro.com/add-and-set-a-new-default-currency-using-pmpro_currencies/

---
+++ checkout/add-new-default-currency.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Add and Set a New Default Currency using $pmpro_currencies
+ *
+ * 
+ * title: Add and Set a New Default Currency using $pmpro_currencies
+ * layout: snippet
+ * collection: checkout
+ * category: currency
+ * 
+ * You can add this recipe to your site by creating a custom plugin
+ * or using the Code Snippets plugin available for free in the WordPress repository.
+ * Read this companion article for step-by-step directions on either method.
+ * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
+ */
+
+function pmpro_currencies_ruble( $currencies ) {
+
+    $currencies['RUB'] = __('Russian Ruble (RUB)', 'pmpro');
+    
+    return $currencies;
+    
+}
+add_filter('pmpro_currencies', 'pmpro_currencies_ruble');
