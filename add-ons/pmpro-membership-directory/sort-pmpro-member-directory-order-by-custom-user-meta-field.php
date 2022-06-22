523d1b6f5e44f4ab4b9522090261164b9238ff30
Andrew Lima <andrew@arctek.co.za>
2022-06-22 15:39:29 +0200

    Rework logic to simplify the gist.

--- add-ons/pmpro-membership-directory/sort-pmpro-member-directory-order-by-custom-user-meta-field.php
+++ add-ons/pmpro-membership-directory/sort-pmpro-member-directory-order-by-custom-user-meta-field.php
@@ -15,25 +15,16 @@
  */
 
 function pmpro_custom_directory_sql_parts( $parts ) {
-
-	// Set your custom registration field name here
-	$user_meta_key_name = 'company';
-
-	// The default is Ascending, set to true to change to a Descending sort order.
-	$switch_sort_order = true;
-
-	/* --- That's it, no further editing required --- */
-
-	$order = $switch_sort_order ? 'DESC' : 'ASC';
-	$user_meta_key_name = esc_sql( $user_meta_key_name );
-
 	global $wpdb;
+    
+	$user_meta_key_name = 'company'; // Custom field name to sort by, use the stored database value.
+	$order = 'ASC'; //Change ASC to DESC if you need to change the order displayed based on meta value.
 
 	$parts['SELECT'] = "SELECT SQL_CALC_FOUND_ROWS u.ID, u.user_login, u.user_email, u.user_nicename, u.display_name, UNIX_TIMESTAMP(u.user_registered) as joindate, mu.membership_id, mu.initial_payment, mu.billing_amount, mu.cycle_period, mu.cycle_number, mu.billing_limit, mu.trial_amount, mu.trial_limit, UNIX_TIMESTAMP(mu.startdate) as startdate, UNIX_TIMESTAMP(mu.enddate) as enddate, m.name as membership, umf.meta_value as first_name, uml.meta_value as last_name, umk.meta_value as user_meta_key_name FROM $wpdb->users u ";
 
-	$parts['JOIN'] .= " LEFT JOIN $wpdb->usermeta umk ON umk.meta_key = '$user_meta_key_name' AND u.ID = umk.user_id ";
+	$parts['JOIN'] .= " LEFT JOIN $wpdb->usermeta umk ON umk.meta_key = '". esc_sql( $user_meta_key_name ) . "' AND u.ID = umk.user_id ";
 
-	$parts['ORDER'] = ' ORDER BY user_meta_key_name ' . $order . ' ';
+	$parts['ORDER'] = ' ORDER BY user_meta_key_name ' . esc_sql( $order ) . ' ';
 
 	return $parts;
 }
