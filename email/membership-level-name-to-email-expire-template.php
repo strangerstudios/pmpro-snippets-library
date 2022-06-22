f266da63a273202a9eaff7e3daa14941d1958026
Andrew Lima <andrew@arctek.co.za>
2022-06-22 15:56:33 +0200

    Update membership-level-name-to-email-expire-template.php

--- email/membership-level-name-to-email-expire-template.php
+++ email/membership-level-name-to-email-expire-template.php
@@ -1,7 +1,12 @@
 <?php
 /**
  * Add !!membership_level_name!! to Paid Memberships Pro expired email template.
- *
+ * 
+ * title: Add !!membership_level_name!! to expired email template.
+ * layout: snippet-example
+ * collection: email
+ * category: custom-fields, email
+ * 
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
  * Read this companion article for step-by-step directions on either method.
@@ -20,8 +25,10 @@ function my_pmpro_email_data_membership_level_name( $data, $email ) {
 				global $wpdb;
 				// Get the last level ID with expired status
 				$last_expired_level = $wpdb->get_var( $wpdb->prepare( "SELECT membership_id FROM $wpdb->pmpro_memberships_users WHERE user_id = %s AND status = %s ORDER BY id DESC LIMIT 1", $user->ID, 'expired' ) );
+				$last_expired_level_obj = pmpro_getLevel( $last_expired_level );
+
 				// Get level name and add to email data
-				$data['membership_level_name'] = pmpro_getLevel( $last_expired_level )->name;
+				$data['membership_level_name'] = $last_expired_level_obj->name;
 			} else {
 				$data['membership_level_name'] = 'N/A';
 			}
