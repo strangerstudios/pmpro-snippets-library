abd2bc82576eda2ad12c3bee37783e235907c07a
Andrew Lima <andrew@arctek.co.za>
2022-08-11 12:56:41 +0200

    WPCS and doc header

--- add-ons/pmpro-mailchimp/send-shipping-fields-mailchimp.php
+++ add-ons/pmpro-mailchimp/send-shipping-fields-mailchimp.php
@@ -1,12 +1,11 @@
-<?php //do not copy
-
+<?php
 /**
  * This recipe will send shipping fields to Mailchimp.
-
- title: send shipping fields to Mailchimp
- layout: Snippets
- collection: pmpro-MailChimp
- category: shipping-fields 
+ *
+ * title: send shipping fields to Mailchimp
+ * layout: snippets
+ * collection: pmpro-mailchimp
+ * category: shipping-fields, email, mailchimp
  *
  * The Shipping Address on Membership Checkout Add On is required for this recipe to work
  * https://www.paidmembershipspro.com/add-ons/shipping-address-membership-checkout/
@@ -18,9 +17,8 @@
  * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
  */
 
-/*
- * Creates Shipping fields in MailChimp
- */
+
+// Creates Shipping fields in MailChimp
 function my_pmpro_mailchimp_merge_field_shipping( $merge_fields ) {
 
 	$merge_fields[] = array( 'name' => 'SFIRSTNAME', 'type' => 'text' );
@@ -33,31 +31,28 @@ function my_pmpro_mailchimp_merge_field_shipping( $merge_fields ) {
 	$merge_fields[] = array( 'name' => 'SPHONE', 'type' => 'text' );
 	$merge_fields[] = array( 'name' => 'SCOUNTRY', 'type' => 'text' );
 
-
 	return $merge_fields;
 }
 add_filter( 'pmpro_mailchimp_merge_fields', 'my_pmpro_mailchimp_merge_field_shipping' );
 
-/*
- * Populates the Shipping fields in MailChimp
- */
-function my_pmpro_mailchimp_listsubscribe_field_shipping($fields, $user) {
+// Populate the Shipping fields in MailChimp from the shipping fields.
+function my_pmpro_mailchimp_listsubscribe_field_shipping( $fields, $user ) {
 
-	$new_fields =  array(
-		'SFIRSTNAME' => ( isset( $_REQUEST['sfirstname'] ) ) ? $_REQUEST['sfirstname'] : get_user_meta( $user->ID, 'sfirstname', true ),
-		'SLASTNAME' => ( isset( $_REQUEST['slastname'] ) ) ? $_REQUEST['slastname'] : get_user_meta( $user->ID, 'slastname', true ),
-		'SADDRESS1' => ( isset( $_REQUEST['saddress1'] ) ) ? $_REQUEST['saddress1'] : get_user_meta( $user->ID, 'saddress1', true ),
-		'SADDRESS2' => ( isset( $_REQUEST['saddress2'] ) ) ? $_REQUEST['saddress2'] : get_user_meta( $user->ID, 'saddress2', true ),
-		'SCITY' => ( isset( $_REQUEST['scity'] ) ) ? $_REQUEST['scity'] : get_user_meta( $user->ID, 'scity', true ),
-		'SSTATE' => ( isset( $_REQUEST['sstate'] ) ) ? $_REQUEST['sstate'] : get_user_meta( $user->ID, 'sstate', true ),
-		'SZIPCODE' => ( isset( $_REQUEST['szipcode'] ) ) ? $_REQUEST['szipcode'] : get_user_meta( $user->ID, 'szipcode', true ),
-		'SPHONE' => ( isset( $_REQUEST['sphone'] ) ) ? $_REQUEST['sphone'] : get_user_meta( $user->ID, 'sphone', true ),
-		'SCOUNTRY' => ( isset( $_REQUEST['scountry'] ) ) ? $_REQUEST['scountry'] : get_user_meta( $user->ID, 'scountry', true ),
+	$new_fields = array(
+		'SFIRSTNAME' => ( isset( $_REQUEST['sfirstname'] ) ) ? sanitize_text_field( $_REQUEST['sfirstname'] ) : get_user_meta( $user->ID, 'sfirstname', true ),
+		'SLASTNAME'  => ( isset( $_REQUEST['slastname'] ) ) ? sanitize_text_field( $_REQUEST['slastname'] ) : get_user_meta( $user->ID, 'slastname', true ),
+		'SADDRESS1'  => ( isset( $_REQUEST['saddress1'] ) ) ? sanitize_text_field( $_REQUEST['saddress1'] ) : get_user_meta( $user->ID, 'saddress1', true ),
+		'SADDRESS2'  => ( isset( $_REQUEST['saddress2'] ) ) ? sanitize_text_field( $_REQUEST['saddress2'] ) : get_user_meta( $user->ID, 'saddress2', true ),
+		'SCITY'      => ( isset( $_REQUEST['scity'] ) ) ? sanitize_text_field( $_REQUEST['scity'] ) : get_user_meta( $user->ID, 'scity', true ),
+		'SSTATE'     => ( isset( $_REQUEST['sstate'] ) ) ? sanitize_text_field( $_REQUEST['sstate'] ) : get_user_meta( $user->ID, 'sstate', true ),
+		'SZIPCODE'   => ( isset( $_REQUEST['szipcode'] ) ) ? sanitize_text_field( $_REQUEST['szipcode'] ) : get_user_meta( $user->ID, 'szipcode', true ),
+		'SPHONE'     => ( isset( $_REQUEST['sphone'] ) ) ? sanitize_text_field( $_REQUEST['sphone'] ) : get_user_meta( $user->ID, 'sphone', true ),
+		'SCOUNTRY'   => ( isset( $_REQUEST['scountry'] ) ) ? sanitize_text_field( $_REQUEST['scountry'] ) : get_user_meta( $user->ID, 'scountry', true )c header.
 	);
 
-	$fields = array_merge($fields, $new_fields);
+	$fields = array_merge( $fields, $new_fields );
 
 	return $fields;
 
 }
-add_action('pmpro_mailchimp_listsubscribe_fields', 'my_pmpro_mailchimp_listsubscribe_field_shipping', 10, 2);
+add_action( 'pmpro_mailchimp_listsubscribe_fields', 'my_pmpro_mailchimp_listsubscribe_field_shipping', 10, 2 );
