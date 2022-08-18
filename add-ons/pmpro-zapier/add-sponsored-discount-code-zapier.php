ae59da3142585bd7300223199e00fae2e6c1504f
SamDaugherty4 <92319050+SamDaugherty4@users.noreply.github.com>
2022-08-18 09:31:35 -0400

    Add Sponsored Discount Code to Zapier
    Adding to Snippet Library to edit: https://www.paidmembershipspro.com/how-to-send-your-sponsored-or-group-members-discount-code-to-zapier/

---
+++ add-ons/pmpro-zapier/add-sponsored-discount-code-zapier.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Add Sponsored discount code to Paid Memberships Pro Zapier Integration data.
+
+ * title: Add sponsored discount code to Zapier
+ * layout: snippet
+ * collection: add-ons, pmpro-zapier
+ * category: discount-code
+ *
+ * This runs on the "After Checkout" trigger. Requires Sponsored/Group Members Add On - https://www.paidmembershipspro.com/add-ons/pmpro-sponsored-members/
+ * Add this code to your site by following
+ */
+
+function my_pmpro_add_sponsored_code_zapier( $data, $user_id, $level, $order ){
+
+    // Bail if Sponsored Members not available.
+    if ( ! function_exists( 'pmprosm_getCodeByUserID' ) ) {
+        return $data;
+    }
+
+   $code_id = pmprosm_getCodeByUserID( $user_id );
+
+    if ( ! empty( $code_id ) ) {
+        $discount_code = pmprosm_getDiscountCodeByCodeID( $code_id );
+        $data['sponsored_code'] = $discount_code->code;
+    }
+
+    return $data;
+}
+add_filter( 'pmproz_after_checkout_data', 'my_pmpro_add_sponsored_code_zapier', 10, 4 );
