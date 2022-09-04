94f4df706dcc8120ae7f76ac021608c6954e7c98
Jason Coleman <jason@strangerstudios.com>
2022-09-04 17:13:25 -0400

    Require Membership to View Certain Taxonomy Terms

---
+++ restricting-content/restrict-by-tags-and-taxonomies.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Require Membership to View Certain Taxonomy Terms
+ * E.g., the example code here locks down a "members" tag.
+ * Note we hook in on priority 15 to run after other filters.
+ *
+ * title: Require Membership to View Certain Taxonomy Terms
+ * layout: snippet
+ * collection: restricting-content
+ * category: content, restriction, tags, terms, taxonomies
+ *
+ * You can add this recipe to your site by creating a custom plugin
+ * or using the Code Snippets plugin available for free in the WordPress repository.
+ * Read this companion article for step-by-step directions on either method.
+ * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
+ */
+function my_require_membership_for_terms( $hasaccess, $post, $user, $post_levels ) {
+	// Make sure we have a post to check.
+	if ( empty( $post ) || empty( $post->ID ) ) {
+		return $hasaccess;
+	}
+	
+	// Make sure we have a user to check.
+	if ( empty( $user ) || empty( $user->ID ) ) {
+		return $hasaccess;
+	}
+	
+	// First array's keys are taxonomy names.
+	// Second array's keys are term slugs.
+	// Second array's values are arrays of level IDs.
+	$term_levels = array(
+		'post_tag' => array(
+			'members' => array( 1, 2, 3 ),
+		),
+	);
+	
+	foreach( $term_levels as $taxonomy => $terms ) {
+		foreach( $terms as $term => $level_ids ) {
+			if( has_term( $term, $taxonomy, $post ) ) {
+				// Post has term, lock it down.
+				$hasaccess = false;
+				if( pmpro_hasMembershipLevel( $level_ids, $user->ID ) ) {
+					// User has level, unlock it again.
+					$hasaccess = true;	// Give access.
+					break 2;			// Break both for loops.
+				}
+			}
+		}
+	}
+	
+	return $hasaccess;
+}
+add_filter( 'pmpro_has_membership_access_filter', 'my_require_membership_for_terms', 15, 4 );
