8ab73d32c12833e827a52d6e40e175c7cb704633
Andrew Lima <andrew@arctek.co.za>
2022-09-13 14:11:02 +0200

    WPCS

--- add-ons/pmpro-affiliates/create-affiliates-bulk.php
+++ add-ons/pmpro-affiliates/create-affiliates-bulk.php
@@ -1,5 +1,4 @@
-<?php //do not copy
-
+<?php
 /**
  * This recipe will generate an affiliate code for all existing users when using PMPro Affiliates.
  * To run this script, you'll need to add /wp-admin/?mypmpro_create_affiliates=true to your URL
@@ -12,54 +11,54 @@
  * collection: add-ons
  * category: pmpro-affiliates
  *
- * Add this code to your PMPro Customizations Plugin - https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
-* Works for PayPal Express and Stripe payment gateways.
-* www.paidmembershipspro.com
-*/
+ * You can add this recipe to your site by creating a custom plugin
+ * or using the Code Snippets plugin available for free in the WordPress repository.
+ * Read this companion article for step-by-step directions on either method.
+ * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
+ */
 
-function mypmpro_run_affiliate_creation(){
+function mypmpro_run_affiliate_creation() {
 
- if( isset( $_REQUEST['mypmpro_create_affiliates'] ) && function_exists( 'mypmpro_create_affiliate' ) ){
+	if ( isset( $_REQUEST['mypmpro_create_affiliates'] ) && function_exists( 'mypmpro_create_affiliate' ) ) {
 
-	 $users = get_users( array( 'role__in' => array( 'subscriber' ) ) );
+		$users = get_users( array( 'role__in' => array( 'subscriber' ) ) );
 
-	 if( $users ){
+		if ( $users ) {
 
-		 foreach( $users as $user ){
+			foreach ( $users as $user ) {
 
-			 mypmpro_create_affiliate( $user->display_name, $user->user_login );
+				mypmpro_create_affiliate( $user->display_name, $user->user_login );
 
-		 }
+			}
 
-		 exit('End');
+			exit( 'End' );
 
-	 }
-
- }
+		}
+	}
 
 }
 add_action( 'admin_init', 'mypmpro_run_affiliate_creation' );
 
-function mypmpro_create_affiliate( $name = '', $username = '' ){
+function mypmpro_create_affiliate( $name = '', $username = '' ) {
 
- if( !function_exists( 'pmpro_affiliates_getNewCode' ) ){
-	 return;
- }
+	if ( ! function_exists( 'pmpro_affiliates_getNewCode' ) ) {
+		return;
+	}
 
- global $wpdb;
+	global $wpdb;
 
- $code = pmpro_affiliates_getNewCode();
+	$code = pmpro_affiliates_getNewCode();
 
- $trackingcode = "";
- $cookiedays = "30"; //Change number of days the cookie should be valid for
- $enabled = true;
+	$trackingcode = '';
+	$cookiedays   = '30'; // Change number of days the cookie should be valid for
+	$enabled      = true;
 
- $sqlQuery = "INSERT INTO $wpdb->pmpro_affiliates (code, name, affiliateuser, trackingcode, cookiedays, enabled) VALUES('" . esc_sql($code) . "', '" . esc_sql($name) . "', '" . esc_sql($username) . "', '" . esc_sql($trackingcode) . "', '" . intval($cookiedays) . "', '" . esc_sql($enabled) . "')";
+	$sqlQuery = "INSERT INTO $wpdb->pmpro_affiliates (code, name, affiliateuser, trackingcode, cookiedays, enabled) VALUES('" . esc_sql( $code ) . "', '" . esc_sql( $name ) . "', '" . esc_sql( $username ) . "', '" . esc_sql( $trackingcode ) . "', '" . intval( $cookiedays ) . "', '" . esc_sql( $enabled ) . "')";
 
- if( $wpdb->query($sqlQuery) !== false ) {
-	 echo "Affiliate Created Successfully - ".$username."<br/>";
- } else {
-	 echo "Affiliate Could Not Be Created  - ".$username."<br/>";
- }
+	if ( $wpdb->query( $sqlQuery ) !== false ) {
+		echo 'Affiliate Created Successfully - ' . $username . '<br/>';
+	} else {
+		echo 'Affiliate Could Not Be Created  - ' . $username . '<br/>';
+	}
 
 }
