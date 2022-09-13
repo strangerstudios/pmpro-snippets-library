20011af39e8d529fcc8c9f5cf247eca218aa852f
Andrew Lima <andrew@arctek.co.za>
2022-09-13 14:13:06 +0200

    WPCS

--- orders/custom-pmpro-order-codes.php
+++ orders/custom-pmpro-order-codes.php
@@ -7,35 +7,35 @@
  * title: Custom order codes for PMPro orders.
  * layout: snippet
  * collection: orders
- * category: custom
+ * category: invoice, invoice-id
  *
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
  * Read this companion article for step-by-step directions on either method.
  * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
  */
- function pmpro_custom_order_codes( $code ) {
+function pmpro_custom_order_codes( $code ) {
 
- 	global $wpdb;
+	global $wpdb;
 
- 	$last_id= $wpdb->get_var( "SELECT `id` FROM $wpdb->pmpro_membership_orders ORDER BY `id` DESC LIMIT 1" );
+	$last_id = $wpdb->get_var( "SELECT `id` FROM $wpdb->pmpro_membership_orders ORDER BY `id` DESC LIMIT 1" );
 
- 	$current_id = (int) $last_id + 1;
+	$current_id = (int) $last_id + 1;
 
- 	$prefix = apply_filters( "pmpro_custom_order_prefix", "INV" ); // You can change "INV" to something else or filter this from another plugin or custom code.
+	$prefix = apply_filters( 'pmpro_custom_order_prefix', 'INV' ); // You can change "INV" to something else or filter this from another plugin or custom code.
 
- 	$code =  $prefix . $current_id; //Code cannot just be an integer and _must_contain_a_string_.
+	$code = $prefix . $current_id; // Code cannot just be an integer and _must_contain_a_string_.
 
- 	// We need to add a check to see if the order code is not taken, otherwise it will be an infinite loop.
- 	$check = $wpdb->get_var( "SELECT `id` FROM $wpdb->pmpro_membership_orders WHERE code = '$code' LIMIT 1" );
+	// We need to add a check to see if the order code is not taken, otherwise it will be an infinite loop.
+	$check = $wpdb->get_var( "SELECT `id` FROM $wpdb->pmpro_membership_orders WHERE code = '$code' LIMIT 1" );
 
- 	// If the code already exists or is only a number, just generate a random order code with 10 digits.
- 	if ( $check || is_numeric( $code ) ) {
- 		$code = wp_generate_password( 10, false, false );
+	// If the code already exists or is only a number, just generate a random order code with 10 digits.
+	if ( $check || is_numeric( $code ) ) {
+		$code = wp_generate_password( 10, false, false );
 
- 	}
+	}
 
- 	return $code;
+	return $code;
 
- }
+}
  add_filter( 'pmpro_random_code', 'pmpro_custom_order_codes', 10, 1 );
