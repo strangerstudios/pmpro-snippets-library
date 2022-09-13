693dec8ce0e04399fa67a497ccb4ecac005b6858
Andrew Lima <andrew@arctek.co.za>
2022-09-13 14:00:14 +0200

    Docblock update.

--- checkout/set-display-name-to-username-new-user-array.php
+++ checkout/set-display-name-to-username-new-user-array.php
@@ -5,11 +5,12 @@
  * title: Display name as entered username created through checkout
  * layout: snippet
  * collection: checkout
- * category: display username
+ * category: username, user
  *
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
- *
+ * Read this companion article for step-by-step directions on either method.
+ * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
  */
 function set_display_name_to_username_new_user_array( $new_user_array ) {
 	$new_user_array['display_name'] = $new_user_array['user_login'];
