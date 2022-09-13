30dd6770158f1556054858afa5eb688a3aa3188f
Andrew Lima <andrew@arctek.co.za>
2022-09-13 14:47:40 +0200

    WPCS

--- add-ons/pmpro-series/limit-posts-pmpro-series.php
+++ add-ons/pmpro-series/limit-posts-pmpro-series.php
@@ -13,19 +13,23 @@
  * Read this companion article for step-by-step directions on either method.
  * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
  */
- function my_pmpro_series_limit_posts_shown( $post_list, $series_object ) {
- 	if ( ! is_user_logged_in() ) {
- 		return $post_list;
- 	}
- 	global $current_user;
- 	$member_days = pmpro_getMemberDays( $current_user->ID );
- 	$posts_to_display = array();
- 	foreach ( $post_list as $sp ) {
- 		$posts_to_display[] = $sp;
- 		if ( max( 0, $member_days ) < $sp->delay ) {
- 			break;
- 		}
- 	}
- 	return $posts_to_display;
- }
+function my_pmpro_series_limit_posts_shown( $post_list, $series_object ) {
+	global $current_user;
+
+	if ( ! is_user_logged_in() ) {
+		return $post_list;
+	}
+
+	$member_days      = pmpro_getMemberDays( $current_user->ID );
+	$posts_to_display = array();
+
+	foreach ( $post_list as $sp ) {
+		$posts_to_display[] = $sp;
+		if ( max( 0, $member_days ) < $sp->delay ) {
+			break;
+		}
+	}
+	
+	return $posts_to_display;
+}
  add_filter( 'pmpro_series_post_list_posts', 'my_pmpro_series_limit_posts_shown', 10, 2 );
