61a5aac96d3c6dd1d548ed26b97f68e3dec836cd
Andrew Lima <andrew@arctek.co.za>
2022-09-13 14:22:05 +0200

    WPCS

--- add-ons/pmpro-membership-directory/pmpro-directory-sort-last-name.php
+++ add-ons/pmpro-membership-directory/pmpro-directory-sort-last-name.php
@@ -5,7 +5,7 @@
  * title: Sort members in Member Directory by last name, first name.
  * layout: snippet
  * collection: add-ons, pmpro-membership-directory
- * category: directory
+ * category: directory, sort, SQL
  *
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
@@ -13,8 +13,8 @@
  * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
  */
 
- function pmpro_custom_directory_sql_parts( $parts ) {
- 	$parts['ORDER'] = ' ORDER BY last_name,first_name ASC ';
- 	return $parts;
- }
+function pmpro_custom_directory_sql_parts( $parts ) {
+	$parts['ORDER'] = ' ORDER BY last_name,first_name ASC ';
+	return $parts;
+}
  add_filter( 'pmpro_member_directory_sql_parts', 'pmpro_custom_directory_sql_parts', 10, 1 );
