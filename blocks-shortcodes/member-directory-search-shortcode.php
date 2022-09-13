f72145da3cc4b3c6c6fdaf911619f8da85358f8c
Andrew Lima <andrew@arctek.co.za>
2022-09-13 14:29:33 +0200

    WPCS

--- blocks-shortcodes/member-directory-search-shortcode.php
+++ blocks-shortcodes/member-directory-search-shortcode.php
@@ -6,43 +6,43 @@
  * title: Create search member directory shortcode
  * layout: snippet
  * collection: block-shortcodes
- * category: member-directory
+ * category: member-directory, search
  *
  *
-* You can add this recipe to your site by creating a custom plugin
-* or using the Code Snippets plugin available for free in the WordPress repository.
-* Read this companion article for step-by-step directions on either method.
-* https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
-*/
+ * You can add this recipe to your site by creating a custom plugin
+ * or using the Code Snippets plugin available for free in the WordPress repository.
+ * Read this companion article for step-by-step directions on either method.
+ * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
+ */
 
-function mypmpro_member_search(){
+function mypmpro_member_search() {
 
- $url = 'https://mysite.com/member-directory'; //Change this line to the member directory URL
+	$url = 'https://mysite.com/member-directory'; // Change this line to the member directory URL
 
- $ret = "";
+	$ret = '';
 
- if(isset($_REQUEST['limit'])){
-	 $limit = intval($_REQUEST['limit']);
- } else if(empty($limit)){
-	 $limit = 15;
- }
+	if ( isset( $_REQUEST['limit'] ) ) {
+		$limit = intval( $_REQUEST['limit'] );
+	} elseif ( empty( $limit ) ) {
+		$limit = 15;
+	}
 
- if( !empty( $_REQUEST['ps'] ) ){
-	 $ps =  stripslashes( esc_attr( $_REQUEST['ps'] ) );
- } else {
-	 $ps = '';
- }
+	if ( ! empty( $_REQUEST['ps'] ) ) {
+		$ps = stripslashes( esc_attr( $_REQUEST['ps'] ) );
+	} else {
+		$ps = '';
+	}
 
- $ret .= '<form role="search" method="post" class="pmpro_member_directory_search search-form" action="'.$url.'">';
-	 $ret .= '<label>';
-		 $ret .= '<span class="screen-reader-text">'.__('Search for:','pmpromd').'</span>';
-		 $ret .= '<input type="search" class="search-field" placeholder="'.__('Search Members','pmpromd').'" name="ps" value="'.$ps.'" title="'.__('Search Members','pmpromd').'" />';
-		 $ret .= '<input type="hidden" name="limit" value="'.esc_attr($limit).'" />';
-	 $ret .= '</label>';
-	 $ret .= '<input type="submit" class="search-submit" value="'.__('Search Members','pmpromd').'">';
- $ret .= '</form>';
+	$ret      .= '<form role="search" method="post" class="pmpro_member_directory_search search-form" action="' . $url . '">';
+	 $ret     .= '<label>';
+		 $ret .= '<span class="screen-reader-text">' . __( 'Search for:', 'pmpromd' ) . '</span>';
+		 $ret .= '<input type="search" class="search-field" placeholder="' . __( 'Search Members', 'pmpromd' ) . '" name="ps" value="' . $ps . '" title="' . __( 'Search Members', 'pmpromd' ) . '" />';
+		 $ret .= '<input type="hidden" name="limit" value="' . esc_attr( $limit ) . '" />';
+	 $ret     .= '</label>';
+	 $ret     .= '<input type="submit" class="search-submit" value="' . __( 'Search Members', 'pmpromd' ) . '">';
+	$ret      .= '</form>';
 
- return $ret;
+	return $ret;
 
 }
 add_shortcode( 'pmpro_member_search', 'mypmpro_member_search' );
