fd4b276e08d726c20b67a18aef2df991f34f3bd9
Andrew Lima <andrew@arctek.co.za>
2022-09-13 14:42:24 +0200

    Move to misc folder instead and WPCS

--- misc/my-pmpro-assign-level-to-role.php
+++ misc/my-pmpro-assign-level-to-role.php
@@ -4,8 +4,8 @@
  *
  * title: Assing level to unique role.
  * layout: snippet
- * collection: admin-pages
- * category: assign-level
+ * collection: misc
+ * category: assign-level, user-role, change-level
  *
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
@@ -13,14 +13,11 @@
  * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
  */
 
-
-function assign_pmpro_level_to_role($user_id, $role, $old_roles)
-{
-	//we found a role related to pmpro level
-	if($role == "yourrole")
-	{
-		pmpro_changeMembershipLevel(1, $user_id);
+function assign_pmpro_level_to_role( $user_id, $role, $old_roles ) {
+	// we found a role related to pmpro level
+	if ( $role == 'yourrole' ) {
+		pmpro_changeMembershipLevel( 1, $user_id );
 	}
 }
 
-add_action('set_user_role', 'assign_pmpro_level_to_role', 10, 3);
+add_action( 'set_user_role', 'assign_pmpro_level_to_role', 10, 3 );
