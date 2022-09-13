b3948d12b791bf924d0f8795b16246929bc80ff0
Andrew Lima <andrew@arctek.co.za>
2022-09-13 14:50:39 +0200

    WPCS

--- payment-gateways/authorize.net/authorizenet-sent-ip.php
+++ payment-gateways/authorize.net/authorizenet-sent-ip.php
@@ -5,7 +5,7 @@
  * title: Send user's IP to authorize.net for fraud detection.
  * layout: snippet
  * collection: payment-gateways, authorize.net
- * category: IP
+ * category: IP, anti-fraud
  *
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
@@ -13,8 +13,8 @@
  * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
  */
 
- function my_pmpro_authorizenet_post_values_send_ip( $post_values ) {
-   $post_values['x_customer_ip'] = $_SERVER['REMOTE_ADDR'];
-   return $post_values;
- }
- add_filter( 'pmpro_authorizenet_post_values', 'my_pmpro_authorizenet_post_values_send_ip' );
+function my_pmpro_authorizenet_post_values_send_ip( $post_values ) {
+	$post_values['x_customer_ip'] = $_SERVER['REMOTE_ADDR'];
+	return $post_values;
+}
+add_filter( 'pmpro_authorizenet_post_values', 'my_pmpro_authorizenet_post_values_send_ip' );
