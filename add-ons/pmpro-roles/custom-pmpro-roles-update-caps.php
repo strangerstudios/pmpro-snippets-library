d1a16fca127bee28038b16035e5c3defe09037ea
Andrew Lima <andrew@arctek.co.za>
2023-02-09 12:42:35 +0200

    Add some security to this one so if people forget to leave it in.

--- add-ons/pmpro-roles/custom-pmpro-roles-update-caps.php
+++ add-ons/pmpro-roles/custom-pmpro-roles-update-caps.php
@@ -6,6 +6,7 @@
  * layout: snippet
  * collection: add-ons, pmpro-roles
  * category: capabilities
+ * license: GPLv3
  *
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
@@ -13,6 +14,17 @@
  * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
  */
 function custom_pmpro_roles_update_caps() {
+
+	// Only logged-in users that are allowed to edit users can run this script.
+	if ( ! current_user_can( 'edit_users') ) {
+		return;
+	}
+
+	// Add ?pmpro_update_role_caps=1 to your URL to run the snippet.
+	if ( ! isset( $_REQUEST['pmpro_update_role_caps'] ) ) {
+		return;
+	}
+
 	$role = get_role( 'pmpro_role_30' );
 	$role->add_cap( 'delete_posts', true );
 	$role->add_cap( 'edit_posts', true );
