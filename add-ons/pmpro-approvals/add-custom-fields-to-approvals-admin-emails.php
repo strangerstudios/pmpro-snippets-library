a7a9b3351aa8f3d7b81c1798ce7dc113ecd79b9a
Kim Coleman <kim@strangerstudios.com>
2023-02-13 08:32:24 -0500

    Adding recipe for approval admin email user fields

---
+++ add-ons/pmpro-approvals/add-custom-fields-to-approvals-admin-emails.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Add custom user fields to the email admins receive when approving the member.
+ * Create User Fields inside Paid Memberships Pro first, then update/duplicate
+ * line 19 with your field names.
+ *
+ * title: Add custom user fields to Approvals emails for admins.
+ * layout: snippet
+ * collection: add-ons
+ * category: pmpro-approvals
+ *
+ * You can add this recipe to your site by creating a custom plugin
+ * or using the Code Snippets plugin available for free in the WordPress repository.
+ * Read this companion article for step-by-step directions on either method.
+ * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
+ */
+
+function my_pmpro_add_user_fields_to_admin_approval_emails( $data, $member, $admin ) {
+    $data['company'] = $member->company; // Update/duplicate this line for your user field names.
+    return $data;
+}
+add_filter( 'pmpro_approvals_admin_approved_email_data', 'my_pmpro_add_user_fields_to_admin_approval_emails', 10, 3 );
+add_filter( 'pmpro_approvals_admin_pending_email_data', 'my_pmpro_add_user_fields_to_admin_approval_emails', 10, 3 );
+add_filter( 'pmpro_approvals_admin_denied_email_data', 'my_pmpro_add_user_fields_to_admin_approval_emails', 10, 3 );
