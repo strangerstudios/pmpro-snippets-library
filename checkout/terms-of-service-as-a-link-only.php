58842e24293d04b297a00f278bf18105b56c9f35
Andrew Lima <andrew@arctek.co.za>
2023-04-27 13:41:25 +0200

    Cleaned up code slightly.
    Slightly cleaned up code and reduced some checks. It's good to check, in this case we should make function names more specific to what this does and there shouldn't be overlap between snippets.

--- checkout/terms-of-service-as-a-link-only.php
+++ checkout/terms-of-service-as-a-link-only.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Add a Terms & Conditions checkbox on checkout with a link to either a media file, e.g. PDF, or page.
+ * Add a Terms & Conditions checkbox on checkout with a link to either a media file (e.g PDF).
  *
  * For a CSS example to enhance the appearance of the checkbox and link, 
  * see this companion gist to get started:
@@ -16,9 +16,7 @@
  * Read this companion article for step-by-step directions on either method.
  * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
  */
-// Declare function if it does not already exist.
-if ( ! function_exists( 'my_pmpro_init_require_tos_link' ) ) {
-	function my_pmpro_init_require_tos_link() {
+function my_pmpro_init_require_tos_link_to_media() {
 
 		// Check if PMPro Core is active
 		if ( ! function_exists( 'pmpro_add_user_field' ) ) {
@@ -30,22 +28,12 @@ if ( ! function_exists( 'my_pmpro_init_require_tos_link' ) ) {
 			return;
 		}
 
-		/**
-		 * Get the link to the Terms & Conditions.
-		 *
-		 * To link to an existing page on your site, instead of a media file,
-		 * you may replace this line:
-		 *
-		 * $tos_link = wp_get_attachment_url( 73 ); // Set your media file ID here.
-		 *
-		 * with this
-		 *
-		 * $tos_link = get_permalink( 177 ); // Set Terms & Conditions post ID here.
-		 */
+
+		// To link to a page, use get_permalink( $page_id )
 		$tos_link = wp_get_attachment_url( 73 ); // Set your media file ID here.
 
 		// Create label that displays a link to the Terms & Conditions.
-		$label = '&nbsp;&nbsp;&nbsp;&nbsp; I agree to the <a href="' . $tos_link . '" target="_blank" id="my-pmpro-require-tos-link" class="my-pmpro-require-tos-link">Terms & Conditions</a>.';
+		$label = 'I agree to the <a href="' . $tos_link . '" target="_blank" id="my-pmpro-require-tos-link" class="my-pmpro-require-tos-link">Terms & Conditions</a>.';
 
 		// Create fields array.
 		$fields = array();
@@ -66,9 +54,4 @@ if ( ! function_exists( 'my_pmpro_init_require_tos_link' ) ) {
 			pmpro_add_user_field( 'before_submit_button', $field );
 		}
 	}
-}
-
-// Add action
-if ( ! has_action( 'init', 'my_pmpro_init_require_tos_link' ) ) {
-	add_action( 'init', 'my_pmpro_init_require_tos_link' );
-}
+add_action( 'init', 'my_pmpro_init_require_tos_link_to_media' );
