69aca066730d277b9f3b6709f7126ccb31797242
Andrew Lima <andrew@arctek.co.za>
2023-04-27 13:42:01 +0200

    Fix spacing.

--- checkout/terms-of-service-as-a-link-only.php
+++ checkout/terms-of-service-as-a-link-only.php
@@ -18,40 +18,40 @@
  */
 function my_pmpro_init_require_tos_link_to_media() {
 
-		// Check if PMPro Core is active
-		if ( ! function_exists( 'pmpro_add_user_field' ) ) {
-			return;
-		}
-
-		// Check that display TOS Setting is disabled.
-		if ( ! empty( pmpro_getOption( 'tospage' ) ) ) {
-			return;
-		}
-
-
-		// To link to a page, use get_permalink( $page_id )
-		$tos_link = wp_get_attachment_url( 73 ); // Set your media file ID here.
-
-		// Create label that displays a link to the Terms & Conditions.
-		$label = 'I agree to the <a href="' . $tos_link . '" target="_blank" id="my-pmpro-require-tos-link" class="my-pmpro-require-tos-link">Terms & Conditions</a>.';
-
-		// Create fields array.
-		$fields = array();
-
-		// Terms & Conditions Checkbox field.
-		$fields[] = new PMPro_Field(
-			'pmpro_tos_checked',
-			'checkbox',
-			array(
-				'label'          => $label,
-				'required'       => true, // make field required
-				'memberslistcsv' => true, // include in memberslist csv
-				'profile'        => false, // do not display on user profile page
-			)
-		);
-
-		foreach ( $fields as $field ) {
-			pmpro_add_user_field( 'before_submit_button', $field );
-		}
+	// Check if PMPro Core is active
+	if ( ! function_exists( 'pmpro_add_user_field' ) ) {
+		return;
 	}
+
+	// Check that display TOS Setting is disabled.
+	if ( ! empty( pmpro_getOption( 'tospage' ) ) ) {
+		return;
+	}
+
+
+	// To link to a page, use get_permalink( $page_id )
+	$tos_link = wp_get_attachment_url( 73 ); // Set your media file ID here.
+
+	// Create label that displays a link to the Terms & Conditions.
+	$label = 'I agree to the <a href="' . $tos_link . '" target="_blank" id="my-pmpro-require-tos-link" class="my-pmpro-require-tos-link">Terms & Conditions</a>.';
+
+	// Create fields array.
+	$fields = array();
+
+	// Terms & Conditions Checkbox field.
+	$fields[] = new PMPro_Field(
+		'pmpro_tos_checked',
+		'checkbox',
+		array(
+			'label'          => $label,
+			'required'       => true, // make field required
+			'memberslistcsv' => true, // include in memberslist csv
+			'profile'        => false, // do not display on user profile page
+		)
+	);
+
+	foreach ( $fields as $field ) {
+		pmpro_add_user_field( 'before_submit_button', $field );
+	}
+}
 add_action( 'init', 'my_pmpro_init_require_tos_link_to_media' );
