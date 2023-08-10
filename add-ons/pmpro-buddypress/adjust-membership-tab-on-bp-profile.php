c8465a27b306ff08c416f3f93b635e474cfa5d07
SamDaugherty4 <92319050+SamDaugherty4@users.noreply.github.com>
2023-08-10 09:51:23 -0400

    Update adjust-membership-tab-on-bp-profile.php

--- add-ons/pmpro-buddypress/adjust-membership-tab-on-bp-profile.php
+++ add-ons/pmpro-buddypress/adjust-membership-tab-on-bp-profile.php
@@ -2,6 +2,7 @@
 
 /**
  * Adjust the output of the "Membership" tab for the BuddyPress Profile.
+ * Learn more at https://www.paidmembershipspro.com/customize-buddypress-membership-tab/
  * You can see what sections to add or remove from here: 
  * https://www.paidmembershipspro.com/documentation/shortcodes/page-shortcodes/#account
  * This recipe adds only the Membership and Invoices section of the PMPro account page to the Membership Tab area on the BP profile page
