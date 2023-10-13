c8bfcf375adff55b0a4580427c2b4f985f08340a
Jarryd Long <JarrydLong@users.noreply.github.com>
2023-10-13 15:12:38 +0200

    Create your own member RSS feed

---
+++ misc/add-member-rss-feed.php
@@ -0,0 +1,31 @@
+<?php
+/** 
+ * Create your own member RSS feed
+ * 
+ * Extra feeds format:
+ * $feeds["Label"] = Feed URL
+ *
+ * title: Create your own member RSS feed
+ * layout: snippet
+ * collection: misc
+ * category: rss, rss-feed
+ *
+ * You can add this recipe to your site by creating a custom plugin
+ * or using the Code Snippets plugin available for free in the WordPress repository.
+ * Read this companion article for step-by-step directions on either method.
+ * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
+ */
+
+function my_pmpromrss_feeds( $feeds ) {
+
+    //recent posts by current user
+    global $current_user;
+    $feeds['My Recent Posts'] = pmpromrss_url( home_url( '/author/' . $current_user->user_login . '/feed/' ) );
+
+    //recent posts from "members" category
+    $feeds['Members Feed'] = pmpromrss_url( home_url( '/category/members/feed' ) );
+
+    return $feeds;
+
+}
+add_action('pmpromrss_feeds', 'my_pmpromrss_feeds');
