1c50959f1e451782b149818de186c586469b30dd
maximilianoRicoTabo <maximilian.rico@gmail.com>
2024-01-11 12:02:21 -0300

    * Remove duplicated * Better assign tags to the return array

--- add-ons/pmpro-google-analytics/capture-unique-post-data.php
+++ add-ons/pmpro-google-analytics/capture-unique-post-data.php
@@ -24,22 +24,14 @@ function my_pmproga4_default_custom_dimension( $dimensions ) {
 	//Change the post ID to the ID of your unique post
 	$my_unique_id = 287;
 	if ( is_single( $my_unique_id ) ) {
-		//change the post ID to the ID of your unique post
+		//Get the WP_Post object itself
 		$my_unique_post = get_post( $my_unique_id );
 		//Get the tag from the post
 		$posttags = get_the_tags( $my_unique_post );
 
-		// Use wp_list_pluck to get an array of tag names
-		$tag_names = wp_list_pluck($posttags, 'name');
-
-		$dimensions = array(
-			'post_type' => '',
-			'author' => '',
-			'category' => '',
-			'tags' => $tag_names,
-		);
-
-		$dimensions =  array ('post_type' => '',  'author' => '', 'category' => '', 'tags' => $posttags );
+		//Use wp_list_pluck to get an array of tag names
+		$tag_names = wp_list_pluck( $posttags, 'name' );
+		$dimensions['tags'] = $tag_names;
 	}
 	return $dimensions;
 }
