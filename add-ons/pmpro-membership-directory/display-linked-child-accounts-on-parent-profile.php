002474d156366b9fed382751d774556c701fc0bb
Andrew Lima <andrew@arctek.co.za>
2024-01-11 12:31:59 +0200

    Minor WPCS updates

--- add-ons/pmpro-membership-directory/display-linked-child-accounts-on-parent-profile.php
+++ add-ons/pmpro-membership-directory/display-linked-child-accounts-on-parent-profile.php
@@ -22,53 +22,52 @@
  * 
  */
 function show_group_account_child_pmpro_member_profile_after( $pu ) {
-	//Bail if pmpro groups account Add On isn't active.
-	if (! class_exists( 'PMProGroupAcct_Group' ) ) {
+	// Bail if pmpro groups account Add On isn't active.adad
+	if ( ! class_exists( 'PMProGroupAcct_Group' ) ) {
 		return;
 	}
 
-	//Get Profile User levels.
+	// Get Profile User levels.
 	$pu_levels = pmpro_getMembershipLevelsForUser( $pu->ID );
 
-	//Bail if user does not have a level
+	// Bail if user does not have a level
 	if ( empty( $pu_levels ) ) {
 		return;
 	}
 
-	//Get the groups for each user's level. 
-	$groups = [];
+	// Get the groups for each user's level.
+	$groups = array();
 	foreach ( $pu_levels as $pu_level ) {
 		$group = PMProGroupAcct_Group::get_group_by_parent_user_id_and_parent_level_id( $pu->ID, $pu_level->id );
-		if( ! empty( $group ) ) {
+		if ( ! empty( $group ) ) {
 			array_push( $groups, $group );
 		}
 	}
 
-	//Bail if the user isn't a parent.
+	// Bail if the user isn't a parent.
 	if ( empty( $groups ) ) {
 		return;
 	}
 
-	//Get the active members for each group.
-	$active_members = [];
+	// Get the active members for each group.
+	$active_members = array();
 	foreach ( $groups as $group ) {
 		$active_members = array_merge( $active_members, $group->get_active_members() );
 	}
- 
-	//Bail if the group has no  members.
+
+	// Bail if the group has no  members.
 	if ( empty( $active_members ) ) {
 		return;
 	}
- 
- 
-	//Display the group members.
-	echo '<strong>Group Account Members</strong>';
+
+	// Display the group members.
+	echo '<strong>' . esc_html__( 'Group Account Members' ) . '</strong>';
 	echo '<ul>';
 	foreach ( $active_members as $active_member ) {
-		$user  = get_userdata( $active_member->group_child_user_id );
+		$user = get_userdata( $active_member->group_child_user_id );
 		echo '<li>' . esc_html( $user->display_name ) . '</li>';
 	}
 	echo '</ul>';
 
 }
-add_action( 'pmpro_member_profile_after', 'show_group_account_child_pmpro_member_profile_after', 1, 10 );
+add_action( 'pmpro_member_profile_after', 'show_group_account_child_pmpro_member_profile_after' );
