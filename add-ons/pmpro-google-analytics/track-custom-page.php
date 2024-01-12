1b9561fe3583a09815c3db2b740c6fc87a58ed9a
Andrew Lima <andrew@arctek.co.za>
2024-01-12 11:57:26 +0200

    Update track-custom-page.php
    Adjusted the order of functions and renamed one function to be more clear what it does.

--- add-ons/pmpro-google-analytics/track-custom-page.php
+++ add-ons/pmpro-google-analytics/track-custom-page.php
@@ -13,7 +13,19 @@
  * Read this companion article for step-by-step directions on either method.
  * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
  */
- 
+
+/**
+ * Add the level id of the level you want to track to the array of level ids to track. (See function below)
+ *
+ * @param array $track_levels The default array of level ids to track.
+ * @return array $track_levels The custom array of level ids to track. 
+ */ 
+function my_pmproga4_track_level_ids_for_page( $track_levels ) {
+	//add the levels id of the level you want to track
+	$track_levels = array( 1, 2, 8 );
+	return $track_levels;
+}
+
 /**
  * Hook into pmproga4_track_view_item_event to track a custom page.
  *
@@ -25,21 +37,9 @@ function my_pmproga4_track_view_item_event() {
 	//Whether or not is the page we wan't to track.
 	$is_custom_page = is_page( $custom_page_to_track );
 	if( $is_custom_page ) {
-		add_filter( 'pmproga4_track_level_ids', 'my_pmproga4_track_level_ids', 10, 2 );
+		add_filter( 'pmproga4_track_level_ids', 'my_pmproga4_track_level_ids_for_page', 10, 2 );
 	}
 	return $is_custom_page;
 }
 
 add_filter( 'pmproga4_track_view_item_event', 'my_pmproga4_track_view_item_event', 10, 2 );
-
-/**
- * Add the level id of the level you want to track to the array of level ids to track.
- *
- * @param array $track_levels The default array of level ids to track.
- * @return array $track_levels The custom array of level ids to track. 
- */ 
-function my_pmproga4_track_level_ids( $track_levels ) {
-	//add the levels id of the level you want to track
-	$track_levels = array( 1, 2, 8 );
-	return $track_levels;
-}
