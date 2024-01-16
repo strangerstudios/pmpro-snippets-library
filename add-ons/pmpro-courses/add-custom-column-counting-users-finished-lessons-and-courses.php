b8f11b68c4cd643b19790abe0deeef57c16a8603
SamDaugherty4 <92319050+SamDaugherty4@users.noreply.github.com>
2024-01-16 10:32:48 -0500

    Update add-custom-column-counting-users-finished-lessons-and-courses.php

--- add-ons/pmpro-courses/add-custom-column-counting-users-finished-lessons-and-courses.php
+++ add-ons/pmpro-courses/add-custom-column-counting-users-finished-lessons-and-courses.php
@@ -6,6 +6,7 @@
  * layout: snippet
  * collection: add-ons
  * category: wp-list-table
+ * link: http://www.paidmembershipspro.com/user-course-progress
  *
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
