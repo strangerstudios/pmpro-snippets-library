c69e96db65c2bc44a2f0296610c7232eee55a407
David Parker <dlparker1005@gmail.com>
2024-02-26 10:08:37 -0500

    Create show-pmpro-pages-in-search.php

---
+++ frontend-pages/show-pmpro-pages-in-search.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Show PMPro Pages in Site Search
+ * 
+ * title: Show PMPro Pages In Site Search
+ * layout: snippet
+ * collection: frontend-pages
+ * category: search
+ * 
+ * You can add this recipe to your site by creating a custom plugin
+ * or using the Code Snippets plugin available for free in the WordPress repository.
+ * Read this companion article for step-by-step directions on either method.
+ * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
+ */
+function my_remove_pmpro_search_filter_pmpro_pages() {
+	remove_filter( 'pre_get_posts', 'pmpro_search_filter_pmpro_pages' );
+}
+add_action( 'init', 'my_remove_pmpro_search_filter_pmpro_pages', 9 );
