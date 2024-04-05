5837551b999958ecaac59aedd6ebd4c4851ea80d
SamDaugherty4 <92319050+SamDaugherty4@users.noreply.github.com>
2024-04-05 13:14:30 -0400

    Update show-pmpro-pages-in-search.php

--- frontend-pages/show-pmpro-pages-in-search.php
+++ frontend-pages/show-pmpro-pages-in-search.php
@@ -6,6 +6,7 @@
  * layout: snippet
  * collection: frontend-pages
  * category: search
+ * link: https://www.paidmembershipspro.com/show-in-search/
  * 
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
