1a213acd0444283e5f9c5c56059634996bebcf92
David Parker <dlparker1005@gmail.com>
2024-06-21 12:22:31 -0400

    Update lockdown-everything-except-homepage.php

--- restricting-content/lockdown-everything-except-homepage.php
+++ restricting-content/lockdown-everything-except-homepage.php
@@ -12,34 +12,30 @@
  * Read this companion article for step-by-step directions on either method.
  * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
  */
+function my_template_redirect_lock_entire_site(){
+	// If the user has a membership level, we don't need to lock down the site.
+	if ( ! function_exists( 'pmpro_hasMembershipLevel' ) || pmpro_hasMembershipLevel() ) {
+		return;
+	}
 
-// Redirect to login or homepage if user is logged out or not a member
-function my_template_redirect(){
-
-	global $current_user;
-
+	// If the user is on an $okay_pages page, the home page, or the login page, we dont' need to redirect.
 	$okay_pages = array(
-        pmpro_getOption( 'billing_page_id' ), 
-        pmpro_getOption( 'account_page_id' ), 
-        pmpro_getOption( 'levels_page_id' ), 
-        pmpro_getOption( 'checkout_page_id' ), 
-        pmpro_getOption( 'confirmation_page_id' )
+		pmpro_getOption( 'billing_page_id' ), 
+		pmpro_getOption( 'account_page_id' ), 
+		pmpro_getOption( 'levels_page_id' ), 
+		pmpro_getOption( 'checkout_page_id' ), 
+		pmpro_getOption( 'confirmation_page_id' )
     );
+	if ( is_home() || is_page( $okay_pages ) || strpos( $_SERVER['REQUEST_URI'], "login" ) ) {
+		return;
+	}
 	
-	//if the user doesn't have a membership, send them home				
-	if(!$current_user->ID 
-		&& !is_home()
-		&& !is_page( $okay_pages ) 
-		&& !strpos( $_SERVER['REQUEST_URI'], "login" ) )
-	{		
+	// 	The user needs to be redirected.
+	if( is_user_logged_in() ) {
+		wp_redirect( home_url() );
+	} else {		
 		wp_redirect( home_url( "wp-login.php?redirect_to=" . urlencode( $_SERVER['REQUEST_URI'] ) ) );
 	}	
-	elseif( is_page() 
-			&& !is_home()
-			&& !is_page($okay_pages) 
-			&& !$current_user->membership_level->ID)
-	{		
-		wp_redirect( home_url() );
-	}
+	exit;
 }
-add_action( 'template_redirect', 'my_template_redirect' );
+add_action( 'template_redirect', 'my_template_redirect_lock_entire_site' );
