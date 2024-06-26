24fbf150ba4017700165f80c2888355d53b6fb03
Andrew Lima <andrew@arctek.co.za>
2024-06-26 14:02:44 +0200

    Missing period.

--- add-ons/pmpro-limit-post-views/countdown-banner.php
+++ add-ons/pmpro-limit-post-views/countdown-banner.php
@@ -28,7 +28,7 @@ function my_pmprolpv_allow_view_js_countdown_banner( $notification_js, $views_re
 
 	ob_start();
 	?>
-	<div style="padding: 40px; 20px;">You have <strong style="background-color: #B00000; border-radius: 5px; display: inline-block; padding: 0 4px;"><?php echo esc_html($formatted_views); ?></strong> free <?php echo esc_html( $article_text );?> remaining this <?php echo esc_html( $level_period ); ?> <a href="<?php echo esc_url( $login_url ); ?>" title="Log in" style="border-bottom: 1px dotted #CCC; color: #FFF; font-weight: bold; text-decoration: none;">Log in</a> or <a href="<?php echo esc_url( $subscribe_url ); ?>" title="Subscribe now" style="border-bottom: 1px dotted #CCC; color: #FFF; font-weight: bold; text-decoration: none;">Subscribe now</a> for unlimited access.</div>
+	<div style="padding: 40px; 20px;">You have <strong style="background-color: #B00000; border-radius: 5px; display: inline-block; padding: 0 4px;"><?php echo esc_html($formatted_views); ?></strong> free <?php echo esc_html( $article_text );?> remaining this <?php echo esc_html( $level_period ); ?>. <a href="<?php echo esc_url( $login_url ); ?>" title="Log in" style="border-bottom: 1px dotted #CCC; color: #FFF; font-weight: bold; text-decoration: none;">Log in</a> or <a href="<?php echo esc_url( $subscribe_url ); ?>" title="Subscribe now" style="border-bottom: 1px dotted #CCC; color: #FFF; font-weight: bold; text-decoration: none;">Subscribe now</a> for unlimited access.</div>
 	<?php
 	$banner_content = ob_get_clean();
 
