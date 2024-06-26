88dc5971e6e30f9ad9a12240fd2b2ed75bfc1798
Andrew Lima <andrew@arctek.co.za>
2024-06-26 15:23:10 +0200

    Update different-email-per-level-approved.php
    WPCS

--- add-ons/pmpro-approvals/different-email-per-level-approved.php
+++ add-ons/pmpro-approvals/different-email-per-level-approved.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Send a different 'Application Approved' email based on the member's level. 
- * 
- * Note that this does not support email templates. Subject & Body should be updated in the code.
- * 
+ * Send a different 'Application Approved' email based on the member's level.
  *
- * title: Send a different 'Application Approved' email based on the member's level. 
+ * Note that this does not support email templates. Subject & Body should be updated in the code.
+ *
+ *
+ * title: Send a different 'Application Approved' email based on the member's level.
  * layout: snippet
  * collection: add-ons
  * category: pmpro-approvals
@@ -19,27 +19,26 @@
 /**
  * Sends a different 'Application Approved' email based on the member's level.
  *
- * @param array   $email   Email template variables.
+ * @param array $email   Email template variables.
  *
  * @return array
  */
 
- function my_pmpro_approval_email_filter( $email ){
+function my_pmpro_approval_email_filter( $email ) {
 
-	if($email->template == 'application_approved'){
-	
-		if($email->data['membership_id'] == 1 ){
+	if ( $email->template == 'application_approved' ) {
+
+		if ( $email->data['membership_id'] == 1 ) {
 			$email->subject = 'Your Membership 1 Has Been Approved';
-			$email->body = 'This is the content/body of the email for members who are approved for level 1';
+			$email->body    = 'This is the content/body of the email for members who are approved for level 1';
 		}
 
-		if($email->data['membership_id'] == 2 ){
+		if ( $email->data['membership_id'] == 2 ) {
 			$email->subject = 'Your Membership 2 Has Been Approved';
-			$email->body = 'This is the content/body of the email for members who are approved for level 2';
+			$email->body    = 'This is the content/body of the email for members who are approved for level 2';
 		}
-		
 	}
-	
+
 	return $email;
 }
-add_filter('pmpro_email_filter', 'my_pmpro_approval_email_filter', 99, 1);
+add_filter( 'pmpro_email_filter', 'my_pmpro_approval_email_filter', 99, 1 );
