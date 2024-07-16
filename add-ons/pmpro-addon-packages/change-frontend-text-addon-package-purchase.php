5a9121b919da87e71b900729824153656f430571
Kim White <kim@pixelpaper.net>
2024-07-16 13:09:13 -0400

    Change Frontend Text for Add On Packages
    updated to include all changeable text.
    part of this post here.
    
    Post will need to be updated too (instructions refers to specific Lines.)
    https://www.paidmembershipspro.com/change-frontend-text-addon-package-purchase-using-wp-gettext-filter/

---
+++ add-ons/pmpro-addon-packages/change-frontend-text-addon-package-purchase.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Change Front End Text for Addon Packages.
+ *
+ * title: Change Frontend Text for Add On Packages
+ * layout: snippet
+ * collection: addons
+ * category: pmpro-addon-packages
+ * url: 
+ *
+ * You can add this recipe to your site by creating a custom plugin
+ * or using the Code Snippets plugin available for free in the WordPress repository.
+ * Read this companion article for step-by-step directions on either method.
+ * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
+ */
+
+ // Update Your custom text your preferred messages., romove elseif 
+ // condition if you don't want to change that text.
+ function my_gettext_pmproap_changes($translated_text, $text, $domain)
+ {
+     if ($domain == "pmpro-addon-packages" && $text == "Purchase this Content (%s)") {
+         $translated_text = "Your custom text. %s";
+     } elseif ($domain == "pmpro-addon-packages" && $text == "Click here to checkout") {
+         $translated_text = "Your custom text. ";
+     }  elseif ($domain == "pmpro-addon-packages" && $text == "Click here to choose a membership level.") {
+         $translated_text = "Your custom text.";
+     } elseif ($domain == "pmpro-addon-packages" && $text == "Choose a Membership Level") {
+         $translated_text = "Your custom text.";
+     } elseif ($domain == "pmpro-addon-packages" && $text == "Choose a Level") {
+         $translated_text = "Your custom text.";
+     } elseif ($domain == "pmpro-addon-packages" && $text == "Buy Now") {
+         $translated_text = "Your custom text.";
+     }
+ 
+     return $translated_text;
+    }
+    add_filter('gettext', 'my_gettext_pmproap_changes', 10, 3);
