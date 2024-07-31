801c75b93f3a817b0cd58a562bce705ba493006c
Andrew Lima <andrew@arctek.co.za>
2024-07-31 11:15:42 +0200

    Support PMPro V3.1 for Addon Packages and more.

--- add-ons/pmpro-addon-packages/change-frontend-text-addon-package-purchase.php
+++ add-ons/pmpro-addon-packages/change-frontend-text-addon-package-purchase.php
@@ -17,18 +17,24 @@
  // Update Your custom text your preferred messages., romove elseif 
  // condition if you don't want to change that text.
 function my_gettext_pmproap_changes( $translated_text, $text, $domain ) {
-	if ( $domain == 'pmpro-addon-packages' && $text == 'Purchase this Content (%s)' ) {
-		$translated_text = 'Your custom text. %s';
-	} elseif ( $domain == 'pmpro-addon-packages' && $text == 'Click here to checkout' ) {
-		$translated_text = 'Your custom text. ';
-	} elseif ( $domain == 'pmpro-addon-packages' && $text == 'Click here to choose a membership level.' ) {
-		$translated_text = 'Your custom text.';
-	} elseif ( $domain == 'pmpro-addon-packages' && $text == 'Choose a Membership Level' ) {
-		$translated_text = 'Your custom text.';
-	} elseif ( $domain == 'pmpro-addon-packages' && $text == 'Choose a Level' ) {
-		$translated_text = 'Your custom text.';
-	} elseif ( $domain == 'pmpro-addon-packages' && $text == 'Buy Now' ) {
-		$translated_text = 'Your custom text.';
+	if ( $domain == 'pmpro-addon-packages' ) {
+		if ( $text == 'This content requires that you purchase additional access. The price is %s.' ) {
+			$translated_text = 'Your custom text. The price is %s.';
+		} elseif ( $text == 'This content requires that you purchase additional access. The price is %1$s or free for our %2$s members.' ) {
+			$translated_text = 'Your custom text. The price is %1$s for %2$s members.';
+		} elseif ( $text == 'Purchase this Content (%s)' ) {
+			$translated_text = 'Your custom text. %s';
+		} elseif ( $text == 'Click here to checkout' ) {
+			$translated_text = 'Your custom text. ';
+		} elseif ( $text == 'Click here to choose a membership level.' ) {
+			$translated_text = 'Your custom text.';
+		} elseif ( $text == 'Choose a Membership Level' ) {
+			$translated_text = 'Your custom text.';
+		} elseif ( $text == 'Choose a Level' ) {
+			$translated_text = 'Your custom text.';
+		} elseif ( $text == 'Buy Now' ) {
+			$translated_text = 'Your custom text.';
+		}
 	}
 
 	return $translated_text;
