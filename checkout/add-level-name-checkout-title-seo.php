4f5326722aff86aa149da3aa6560700f28debe19
Mary Job <mariaojob@gmail.com>
2024-08-21 21:09:11 +0100

    Create add-level-name-checkout-title-output-seo.php
    add level name to the_title() output on membership checkout page for SEO

---
+++ checkout/add-level-name-checkout-title-seo.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Add Level Name to the_title() Output on Membership Checkout Page for SEO
+ * 
+ * title: Add Level Name to the_title() Output on Membership Checkout Page for SEO
+ * layout: snippet
+ * collection: checkout
+ * category: seo
+ * url: https://www.paidmembershipspro.com/add-level-name-to-membership-checkout-page-title-for-seo/
+ * 
+ * You can add this recipe to your site by creating a custom plugin
+ * or using the Code Snippets plugin available for free in the WordPress repository.
+ * Read this companion article for step-by-step directions on either method.
+ * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
+ */
+
+//add level name to the_title() output on membership checkout page for SEO
+function my_pmpro_membership_checkout_page_title( $title, $id ) {
+	global $pmpro_pages;
+	if(!empty($pmpro_pages) && is_page($pmpro_pages['checkout']) && in_the_loop()) {
+		global $pmpro_level;
+		$title .= ": " . $pmpro_level->name;
+	}
+	return $title;
+}
+
+add_filter( 'the_title', 'my_pmpro_membership_checkout_page_title', 10, 2 );
