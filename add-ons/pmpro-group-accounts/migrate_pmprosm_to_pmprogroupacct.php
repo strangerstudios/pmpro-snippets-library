7683fef52f09ef03fce1d75fee9089a3145cb265
SamDaugherty4 <92319050+SamDaugherty4@users.noreply.github.com>
2024-09-25 09:54:27 -0400

    Update migrate_pmprosm_to_pmprogroupacct.php

--- add-ons/pmpro-group-accounts/migrate_pmprosm_to_pmprogroupacct.php
+++ add-ons/pmpro-group-accounts/migrate_pmprosm_to_pmprogroupacct.php
@@ -20,6 +20,7 @@
  * layout: snippet
  * collection: add-ons, pmpro-group-accounts
  * category: migrate, sponsored members, group accounts
+ * link: https://www.paidmembershipspro.com/migrate-sponsored-members-to-group-accounts/
  *
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
