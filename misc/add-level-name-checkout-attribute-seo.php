6012a1b3b40e5304c9994048227a0d39f8f033ec
Andrew Lima <andrew@arctek.co.za>
2024-10-02 14:30:00 +0200

    Move to 'misc' instead.

--- misc/add-level-name-checkout-attribute-seo.php
+++ misc/add-level-name-checkout-attribute-seo.php
@@ -1,28 +1,24 @@
 <?php
-
 /**
  * Add Level Name to the Membership Checkout <title> Attribute for SEO
- * 
+ *
  * title: Add Level Name to Membership Checkout Attribute for SEO
  * layout: snippet
- * collection: checkout
+ * collection: misc
  * category: seo
  * url: https://www.paidmembershipspro.com/add-level-name-to-membership-checkout-page-title-for-seo/
- * 
+ *
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
  * Read this companion article for step-by-step directions on either method.
  * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
  */
-
-//add level name to membership checkout <title> attribute for SEO
-function my_pmpro_membership_checkout_head_title($title, $sep) {
+function my_pmpro_membership_checkout_head_title( $title, $sep ) {
 	global $pmpro_pages;
-	if(!empty($pmpro_pages) && is_page($pmpro_pages['checkout'])) {
+	if ( ! empty( $pmpro_pages ) && is_page( $pmpro_pages['checkout'] ) ) {
 		global $pmpro_level;
-		$title = $pmpro_level->name . " " . $sep . " " . $title;
+		$title = $pmpro_level->name . ' ' . $sep . ' ' . $title;
 	}
 	return $title;
 }
-
 add_filter( 'wp_title', 'my_pmpro_membership_checkout_head_title', 20, 2 );
