02d0a58c4752d92089214f220b132e79450f84ae
Andrew Lima <andrew@arctek.co.za>
2024-10-02 13:37:41 +0200

    Support multiple level IDs.

--- add-ons/pmpro-group-accounts/my_pmprogroupacct_require-child-level-with-code.php
+++ add-ons/pmpro-group-accounts/my_pmprogroupacct_require-child-level-with-code.php
@@ -1,12 +1,11 @@
 <?php
 /**
- * Restrict a level sign up to require Group code.
+ * Restrict checkout for a level to require a Group Account code.
  * 
- * title: Restrict a level sign up to require Group code. 
+ * title: Restrict checkout for a level to require a Group Account code.
  * layout: snippet
  * collection: add-ons, pmpro-group-accounts
  * category: levels, checkout
- * url: none
  *
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
@@ -18,10 +17,10 @@ function my_pmpro_require_group_code_to_register( $pmpro_continue_registration )
 	if ( ! $pmpro_continue_registration ) {
 		return $pmpro_continue_registration;
 	}
-	// change line 24 to the level you want to restrict.	
-	// if level = 2 and there is no group code, then show an error message
+	
+	$required_levels = array( 2 ); // Add the level IDs that require a group code here.
 	$pmpro_level = pmpro_getLevelAtCheckout();
-	if ( $pmpro_level->id == 2 && empty( $_REQUEST['pmprogroupacct_group_code'] ) ) {
+	if ( in_array( $pmpro_level->id, $required_levels ) && empty( $_REQUEST['pmprogroupacct_group_code'] ) ) {
 		pmpro_setMessage("You must use a valid group code to register for this level.", "pmpro_error");
 		return false;
 	}
