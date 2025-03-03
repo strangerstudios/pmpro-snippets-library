ee9c1a0f6afd330c3fb8bf0574d9985ca9931453
Mary Job <mariaojob@gmail.com>
2025-03-03 18:03:00 +0100

    Update member-count-shortcode.php

--- blocks-shortcodes/member-count-shortcode.php
+++ blocks-shortcodes/member-count-shortcode.php
@@ -20,63 +20,57 @@
  *
  *  @param $attrs An array of attributes ( status, level, justnumber)
  */
+
 function pmpro_member_count_shortcode( $attrs = null ) {
-	global $wpdb;
-	$attrs = shortcode_atts(
-		array(
-			'status' => 'active',
-			'level' => null,
-			'justnumber' => false,
-		),
-		$attrs
-	);
+    global $wpdb;
+    $attrs = shortcode_atts(
+        array(
+            'status' => 'active',
+            'levels' => null,
+            'justnumber' => false,
+        ),
+        $attrs
+    );
+	
+    $statuses = array_map('trim', explode(',', $attrs['status']));
 
-	$statuses = array_map('trim', explode(',', $attrs['status']));
+    if (! is_array($statuses) && ! empty($attrs['status']) ) {
 
-	//avoid pass null
-	if (isset($attrs['level'])) {
-		$levels = array_map('trim', explode(',', $attrs['level']));
-	}
-	if (! is_array($statuses) && ! empty($attrs['status']) ) {
+        $statuses = array($attrs['status']);
+    }
 
-		$statuses = array($attrs['status']);
-	}
+    $sql = "SELECT COUNT(*)
+            FROM {$wpdb->pmpro_memberships_users}
+            WHERE `status` IN ('" . implode("', '", $statuses) . "')";
 
-	$sql = "SELECT COUNT(*)
-			FROM {$wpdb->pmpro_memberships_users}
-			WHERE `status` IN ('" . implode("', '", $statuses) . "')";
 
-	//Ensure isn't empty and values are int
-	if (! empty( $levels ) && array_reduce($levels, function ($result, $item) { return $result && is_int(intval($item)); }, true )
-	) {
-		$sql .= "
-			AND `membership_id` IN ('" . implode("', '", $levels) . "')";
-	}
+    if (! empty($attrs['levels'])) {
+        $sql .= "
+            AND `membership_id` = " . intval($attrs['levels']);
+    }
 
-	$member_count = $wpdb->get_var($sql);
-	//Bail if there's an error
-	if ( is_wp_error($member_count)) {
-		return sprintf( __( "Error while processing request: %s", "pmpromsc" ), esc_html ( $wpdb->print_error()) );
-	}
-	//If it's just number return it
-	if( !empty($attrs['justnumber'] ) ) {
-		return $member_count;
-	} else {
-		//
-		if (! empty($levels ) ) {
-			$level_names="";
-			if (count($levels) === 1) {
-				// If there's only one element, directly assign its name
-				$level_names = pmpro_getLevel($levels[0])->name;
-			} else {
-				foreach ($levels as $level) {
-					$level_names .= pmpro_getLevel($level)->name . ', ';
-				}
-			}
-			return sprintf( __( "This site has %d %s members", "pmpromsc" ), esc_html( $member_count ) ,  esc_html ( $level_names ) ) ;
-		} else {
-			return sprintf( __( "This site has %d members", "pmpromsc" ), esc_html( $member_count ) );
-		}
-	}
+    $member_count = $wpdb->get_var($sql);
+
+    if (!is_wp_error($member_count)) {
+
+        if (! empty($attrs['levels'])) {
+
+            $l = pmpro_getLevel($attrs['levels']);
+            if(!empty($attrs['justnumber']))
+                return $member_count;
+            else
+                return sprintf( __( "This site has %d %s members", "pmpromsc" ), $member_count, $l->name );
+
+        } else {
+            if(!empty($attrs['justnumber']))
+                return $member_count;
+            else
+                return sprintf( __( "This site has %d members", "pmpromsc" ), $member_count );
+        }
+    } else {
+
+        return sprintf( __( "Error while processing request: %s", "pmpromsc" ), $wpdb->print_error() );
+    }
 }
+
 add_shortcode('pmpro_member_count', 'pmpro_member_count_shortcode');
