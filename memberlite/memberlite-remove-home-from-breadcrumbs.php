e56163f5aff8f9509a559238c251034bdb11f175
Kim Coleman <kim@strangerstudios.com>
2025-03-11 19:45:08 -0400

    Adding recipe to hide "home" from Memberlite breadcrumbs

---
+++ memberlite/memberlite-remove-home-from-breadcrumbs.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Remove the "Home" breadcrumb from the Memberlite theme.
+ *
+ * title: Remove Home From Memberlite Breadcrumbs
+ * layout: snippet
+ * collection: memberlite
+ * category: design
+ *
+ * 
+ * You can add this recipe to your site by creating a custom plugin
+ * or using the Code Snippets plugin available for free in the WordPress repository.
+ * Read this companion article for step-by-step directions on either method.
+ * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
+ */
+
+function my_memberlite_hide_home_breadcrumb( $memberlite_hide_home_breadcrumb ) {
+	return true;
+}
+add_filter( 'memberlite_hide_home_breadcrumb', 'my_memberlite_hide_home_breadcrumb' );
