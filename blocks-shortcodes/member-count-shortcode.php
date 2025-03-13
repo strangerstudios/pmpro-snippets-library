6c032fa7343c8b11592907890bb97cd01f5f7509
Andrew Lima <andrew@arctek.co.za>
2025-03-13 10:47:41 +0200

    Refactor logic to make it easier to undersstand.

--- blocks-shortcodes/member-count-shortcode.php
+++ blocks-shortcodes/member-count-shortcode.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * This recipe will create a [pmpro_member_count] shortcode, that will display the number of members in a specific level with a specific status.
  * title: Add a shortcode that counts members. Support passing different statuses
@@ -16,57 +15,55 @@
  */
 
 /**
- *
  * @param $attrs An array of attributes ( status, level, justnumber)
  */
-
 function pmpro_member_count_shortcode( $attrs = null ) {
 	global $wpdb;
 
-	$attrs = shortcode_atts(
-		array(
-			'status'     => 'active',
-			'levels'     => null,
-			'justnumber' => false,
-		),
+	extract(
+		shortcode_atts(
+			array(
+				'status'     => 'active',
+				'level'      => null,
+				'justnumber' => false,
+			),
 		$attrs
+	    )
 	);
 
 	// Process statuses
-	$statuses = array_map( 'trim', explode( ',', $attrs['status'] ) );
+	$statuses = array_map( 'trim', explode( ',', $status ) );
 
-	if ( ! is_array( $statuses ) && ! empty( $attrs['status'] ) ) {
-		$statuses = array( $attrs['status'] );
+	if ( ! is_array( $statuses ) && ! empty( $status ) ) {
+		$statuses = array( $status );
 	}
 
 	// Start building SQL
 	$sql = "SELECT COUNT(*) FROM {$wpdb->pmpro_memberships_users} WHERE `status` IN ('" . implode( "', '", $statuses ) . "')";
 
 	// Process levels
-	if ( ! empty( $attrs['levels'] ) ) {
-		$levels = array_map( 'intval', explode( ',', $attrs['levels'] ) );
+	if ( ! empty( $level ) ) {
+		$levels = array_map( 'intval', explode( ',', $level ) );
 		$sql    .= " AND `membership_id` IN (" . implode( ',', $levels ) . ")";
 	}
 
 	$member_count = $wpdb->get_var( $sql );
+	
+	// There was an error getting data from the database.
+	if ( is_wp_error( $member_count ) ) {
+		return sprintf( esc_html__( "Error while processing request: %s", "pmpromsc" ), $wpdb->print_error() );	
+	}
+	
+	// Display only the number of members if we want the integer value.
+	if ( ! empty( $justnumber ) ) {
+		return $member_count;
+	} 
 
-	if ( ! is_wp_error( $member_count ) ) {
-		if ( ! empty( $attrs['levels'] ) ) {
-			if ( ! empty( $attrs['justnumber'] ) ) {
-				return $member_count;
-			} else {
-				return sprintf( __( "This site has %d members in the selected levels", "pmpromsc" ), $member_count );
-			}
-		} else {
-			if ( ! empty( $attrs['justnumber'] ) ) {
-				return $member_count;
-			} else {
-				return sprintf( __( "This site has %d members", "pmpromsc" ), $member_count );
-			}
-		}
+	// Change the text if we are outputting for specific levels, or the overall member count.
+	if ( ! empty( $level ) ) {
+		return sprintf( esc_html__( "This site has %d members in the selected levels", "pmpromsc" ), $member_count );
 	} else {
-		return sprintf( __( "Error while processing request: %s", "pmpromsc" ), $wpdb->print_error() );
+		return sprintf( esc_html__( "This site has %d members", "pmpromsc" ), $member_count );
 	}
 }
-
 add_shortcode( 'pmpro_member_count', 'pmpro_member_count_shortcode' );
