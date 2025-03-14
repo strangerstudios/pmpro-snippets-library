2cf1ee1c4d25f2a989ac773aab72017dfee6631c
Andrew Lima <andrew@arctek.co.za>
2025-03-14 11:19:28 +0200

    Update format_phone_field.php

--- checkout/format_phone_field.php
+++ checkout/format_phone_field.php
@@ -1,49 +1,52 @@
 <?php
 /**
- * Format a custom field to collect the phone field to look like (###) ###-####
+ * Format the billing phone field to look like (###) ###-####.
+ * You may adjust this code to target custom phone fields if necessary.
  *
- * title: How to Collect a Member Telephone Number at Checkout
+ *
+ * title: Format the billing phone number to be (###) ###-####
  * layout: snippet
  * collection: checkout
- * category: user fields
+ * category: phone, user-fields
  *
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
  * Read this companion article for step-by-step directions on either method.
  * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
  */
-
  function my_pmpro_format_phone() {
 	?>
 	<script>
-		jQuery( document ).ready( function( $ ) {
-			//Change the id below accordingly
-			$( '#phone' ).on( 'input', function() {
-				 let input = $( this ).val();
-
-				// Remove all non-numeric characters
-				input = input.replace( /\D/g, '' );
-
-				// Initialize formatted output
-				let formatted = '';
-
-				// Add area code part
-				if ( input.length > 0 ) {
-					formatted += '(' + input.substring( 0, 3 );
-				}
-				// Add first 3 digits after area code
-				if ( input.length > 3 ) {
-					formatted += ') ' + input.substring( 3, 6 );
-				}
-				// Add last 4 digits
-				if ( input.length > 6 ) {
-					formatted += '-' + input.substring( 6, 10 );
-				}
-
-				// Set the formatted value back to the input field
-				$( this ).val( formatted );
-			});
-		});
+	    jQuery(document).ready(function () {
+	        // Change the id below accordingly.
+	        jQuery('#bphone').on('input', function () {
+	            let input = jQuery(this).val();
+	
+	            // Remove all non-numeric characters
+	            input = input.replace(/\D/g, '');
+	
+	            // Limit input to 10 characters (standard US phone number format)
+	            if (input.length > 10) {
+	                input = input.substring(0, 10);
+	            }
+	
+	            // Format input as (XXX) XXX-XXXX
+	            let formatted = '';
+	
+	            if (input.length > 0) {
+	                formatted = '(' + input.substring(0, Math.min(3, input.length));
+	            }
+	            if (input.length >= 4) {
+	                formatted += ') ' + input.substring(3, Math.min(6, input.length));
+	            }
+	            if (input.length >= 7) {
+	                formatted += '-' + input.substring(6, Math.min(10, input.length));
+	            }
+	
+	            // Set the formatted value back to the input field
+	            jQuery(this).val(formatted);
+	        });
+	    });
 	</script>
 	<?php
 }
