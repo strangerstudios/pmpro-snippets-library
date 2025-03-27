14cd29fe129ce43427191b37da84fd415b9958d5
Kim Coleman <kim@strangerstudios.com>
2025-03-27 09:12:40 -0400

    Example of using the Memberlite Font Awesome Icon shortcode.

---
+++ memberlite/memberlite-shortcodes-example-font-awesome-icon.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Example of using the Memberlite Font Awesome Icon shortcode.
+ * Learn more at https://www.paidmembershipspro.com/documentation/memberlite/memberlite-shortcodes/font-awesome-icons/
+ *
+ * title: Example of the Memberlite Font Awesome Icon Shortcode.
+ * layout: snippet
+ * collection: memberlite
+ * category: shortcodes
+ *
+ */
+?>
+[fa icon="heart" animate="beat"]
+
+[fa icon="sync" animate="spin"]
+
+[fa icon="arrow-right" rotate="90"]
+
+[fa icon="check-circle" color="success" size="lg"]
+
+[fa icon="exclamation-circle" color="error" size="2x"]
