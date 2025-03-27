f6cb98962e3a16540275610d88d8fb82933ac008
Kim Coleman <kim@strangerstudios.com>
2025-03-27 08:58:38 -0400

    Adding example snippt for button shortcode in Memberlite

---
+++ memberlite/memberlite-shortcodes-example-button.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Example of using the Memberlite Button shortcode.
+ * Learn more at https://www.paidmembershipspro.com/documentation/memberlite/memberlite-shortcodes/buttons/
+ *
+ * title: Example of the Memberlite Button Shortcode.
+ * layout: snippet
+ * collection: memberlite
+ * category: shortcodes
+ *
+ */
+?>
+[memberlite_btn style="success" href="/blog/" text="Success Button" target="self"]
+
+[memberlite_btn style="alert" href="/blog/" text="Alert Button" target="self"]
+
+[memberlite_btn style="error" href="/blog/" text="Error Button" target="self"]
+
+[memberlite_btn style="info" href="/blog/" text="Info Button" target="self"]
+
+[memberlite_btn style="link" href="/blog/" text="Link Button" target="self"]
+
+[memberlite_btn style="default" href="/blog/" text="Default Button" target="self"]
+
+[memberlite_btn style="primary" href="/blog/" text="Primary Button" target="self"]
+
+[memberlite_btn style="secondary" href="/blog/" text="Secondary Button" target="self"]
+
+[memberlite_btn style="action" href="/blog/" text="Action Button" target="self"]
