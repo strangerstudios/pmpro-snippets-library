253c560f293aed3647a3246e554eb6dbf868778e
Andrew Lima <andrew@arctek.co.za>
2025-04-04 14:21:16 +0200

    force registering of PMPro shortcodes

--- integration-compatibility/my-pmpro-load-shortcode-for-bricks.php
+++ integration-compatibility/my-pmpro-load-shortcode-for-bricks.php
@@ -14,26 +14,29 @@
  * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
  */
 
-// Check if PMPro is active
-if ( ! defined('PMPRO_VERSION') ) {
-	return;
-}
+ function my_pmpro_force_register_shortcodes() {
+	// Check if PMPro is active
+	if ( ! defined('PMPRO_VERSION') ) {
+		return;
+	}
 
-// This registers PMPro shortcodes to be used in Bricks Builder.
-$pmpro_shortcodes = [
-	'pmpro_levels'      => 'levels',
-	'pmpro_billing'     => 'billing',
-	'pmpro_cancel'      => 'cancel',
-	'pmpro_checkout'    => 'checkout',
-	'pmpro_confirmation'=> 'confirmation',
-	'pmpro_invoice'     => 'invoice'
-];
+	// This registers PMPro shortcodes to be used in Bricks Builder.
+	$pmpro_shortcodes = [
+		'pmpro_levels'      => 'levels',
+		'pmpro_billing'     => 'billing',
+		'pmpro_cancel'      => 'cancel',
+		'pmpro_checkout'    => 'checkout',
+		'pmpro_confirmation'=> 'confirmation',
+		'pmpro_invoice'     => 'invoice'
+	];
 
-foreach ($pmpro_shortcodes as $shortcode => $template) {
-	if (!shortcode_exists($shortcode)) {
-		add_shortcode($shortcode, function() use ($template) {
-			require_once PMPRO_DIR . "/preheaders/{$template}.php";
-			return pmpro_loadTemplate($template, 'local', 'pages');
-		});
+	foreach ( $pmpro_shortcodes as $shortcode => $template ) {
+		if ( ! shortcode_exists( $shortcode ) ) {
+			add_shortcode( $shortcode, function() use ( $template ) {
+				require_once PMPRO_DIR . "/preheaders/{$template}.php";
+				return pmpro_loadTemplate( $template, 'local', 'pages' );
+			});
+		}
 	}
 }
+add_action( 'plugins_loaded', 'my_pmpro_force_register_shortcodes' );
