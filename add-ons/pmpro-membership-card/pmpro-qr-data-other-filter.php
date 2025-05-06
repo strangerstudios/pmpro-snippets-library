6dbffbc22e0bb1e71f31d3ebadbe537ec5eac220
David Mutero <57159280+davidmutero@users.noreply.github.com>
2025-05-06 18:23:21 +0200

    Create pmpro-qr-data-other-filter.php

---
+++ add-ons/pmpro-membership-card/pmpro-qr-data-other-filter.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Customize QR Code Data Based on Membership Status
+ *
+ * This recipe adds dynamic status data ("active" or "inactive") to the QR code 
+ * when using the PMPro Membership Card shortcode with the attribute `qr_data='other'`.
+ *
+ * title: Customize QR Code Data Based on Membership Status
+ * layout: snippet
+ * collection: add-ons, pmpro-membership-card
+ * category: qr-code
+ * link: https://www.paidmembershipspro.com/customize-membership-card-wordpress/
+ *
+ * You can add this recipe to your site by creating a custom plugin
+ * or using the Code Snippets plugin available for free in the WordPress repository.
+ * Read this companion article for step-by-step directions on either method.
+ * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
+ */
+
+function mypmro_membership_card_show_active( $pmpro_membership_card_user, $option ){
+
+	//Checking if PMPro is active
+	if( !function_exists( 'pmpro_hasMembershipLevel' ) ){
+		return;
+	}
+
+	if( $option == 'other' ){
+		//Change according to the levels you'd like to check for
+		$has_level = pmpro_hasMembershipLevel( array( 1, 2, 3 ) );
+
+		if( $has_level ){
+			return 'active';
+		} else {
+			return 'inactive';
+		}
+	}	
+
+}
+add_filter( 'pmpro_membership_card_qr_data_other', 'mypmro_membership_card_show_active', 10, 2 );
