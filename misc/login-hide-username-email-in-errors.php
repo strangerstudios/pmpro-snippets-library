7cc597ca47d29ea2ec06cf94abece2fd38f5b51a
SamDaugherty4 <92319050+SamDaugherty4@users.noreply.github.com>
2025-05-09 13:48:39 -0400

    Update login-hide-username-email-in-errors.php

--- misc/login-hide-username-email-in-errors.php
+++ misc/login-hide-username-email-in-errors.php
@@ -6,7 +6,7 @@
  * layout: snippet
  * collection: misc
  * category: login, error, text change
- * link: TBD
+ * link: https://www.paidmembershipspro.com/customize-login-error-messages-to-boost-account-security/
  * 
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
