82d870bb24cc046867d0bec80dc20f2f76fac1c2
Andrew Lima <andrew@arctek.co.za>
2025-06-30 11:42:19 +0200

    Include blog post link.

--- membership-levels/change-membership-level-on-expiration-only.php
+++ membership-levels/change-membership-level-on-expiration-only.php
@@ -7,6 +7,7 @@
  * layout: snippet
  * collection: membership-levels
  * category: expiration
+ * link: https://www.paidmembershipspro.com/change-membership-level-on-cancellation-or-expiration/
  *
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
