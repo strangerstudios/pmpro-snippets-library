044905bc1d497364e40ecae30c68a13dec07f7da
Kim White <kim@pixelpaper.net>
2025-07-23 09:41:13 -0400

    New Snippet Recurring Payments
    to replace the gist on this post:
    https://www.paidmembershipspro.com/notify-members-of-upcoming-recurring-payment-automatic-renewal-for-membership/

---
+++ email/change-recurring-payment-reminder.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Change recurring payment reminder emails from 7 days to other.
+ *
+ * title: Change Additional Recurring Payment Reminder Emails from 7 days to other
+ * layout: snippet
+ * collection: email
+ * category: recurring reminder, emails
+ * link: https://www.paidmembershipspro.com/notify-members-of-upcoming-recurring-payment-automatic-renewal-for-membership/
+ *
+ * You can add this recipe to your site by creating a custom plugin
+ * or using the Code Snippets plugin available for free in the WordPress repository.
+ * Read this companion article for step-by-step directions on either method.
+ * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
+ */
+
+function my_pmpro_change_recurring_emails( $emails ) {
+
+  // Remove the email that is sent 7 days before subscription payment.
+  unset( $emails[7] );
+  
+  // Send a reminder email to be sent 14 days before the subscription payment.
+  // The email template to be used is 'membership_recurring'.
+  $emails[14] = 'membership_recurring';
+  
+  return $emails;
+  
+  }
+
+  add_filter( 'pmpro_upcoming_recurring_payment_reminder', 'my_pmpro_change_recurring_emails', 10, 1 );
