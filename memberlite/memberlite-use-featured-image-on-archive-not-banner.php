90d6ee80502812ec9b76ea7896840c9d3d708c93
Kim White <kim@pixelpaper.net>
2025-07-30 10:49:09 -0400

    tweak name of function

--- memberlite/memberlite-use-featured-image-on-archive-not-banner.php
+++ memberlite/memberlite-use-featured-image-on-archive-not-banner.php
@@ -7,6 +7,7 @@
  * layout: snippet
  * collection: memberlite
  * category: display, featured image
+ * link: tbd
  * 
  * This recipe prevents the Memberlite theme from replacing the banner image on the homepage
  * with the custom level banner image set for membership levels.
@@ -17,10 +18,10 @@
  * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
  */
 
-function conditionally_remove_memberlite_banner_image_filter() {
+function my_memberliteconditionally_remove_memberlite_banner_image() {
 	if ( is_home() ) {
 		remove_filter( 'memberlite_get_banner_image', 'memberlite_maybe_get_custom_banner_image', 10 );
 	}
 }
-add_action( 'template_redirect', 'conditionally_remove_memberlite_banner_image_filter' );
+add_action( 'template_redirect', 'my_memberliteconditionally_remove_memberlite_banner_image' );
 
