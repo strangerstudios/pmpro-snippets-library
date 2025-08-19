72a6682382189075acbd23c1ff3dcc825509c09e
Andrew Lima <andrew@arctek.co.za>
2025-08-19 15:59:25 +0200

    Minor space changes

--- email/change-recurring-payment-reminder.php
+++ email/change-recurring-payment-reminder.php
@@ -13,18 +13,14 @@
  * Read this companion article for step-by-step directions on either method.
  * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
  */
-
 function my_pmpro_change_recurring_emails( $emails ) {
 
-  // Remove the email that is sent 7 days before subscription payment.
-  unset( $emails[7] );
+	// Remove the email that is sent 7 days before subscription payment.
+	unset( $emails[7] );
   
-  // Change to 14 to send a reminder email to how many days before the subscription payment.
-  // The email template to be used is 'membership_recurring'.
-  $emails[14] = 'membership_recurring';
-  
-  return $emails;
-  
-  }
-
+	// Change to 14 to send a reminder email to how many days before the subscription payment.
+	// The email template to be used is 'membership_recurring'.
+	$emails[14] = 'membership_recurring';
+	return $emails;
+}
   add_filter( 'pmpro_upcoming_recurring_payment_reminder', 'my_pmpro_change_recurring_emails', 10, 1 );
