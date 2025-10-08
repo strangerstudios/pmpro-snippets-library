99f426b3bcaad83e5977f40a4bf111feb046758a
Kim White <kim@pixelpaper.net>
2025-10-08 09:27:31 -0400

    Require a Stronger Password Score for Member Registration
    New Snippet, Example of how to use the pmprosp_minimum_password_score filter
    
    Will be linked to the add on page

---
+++ add-ons/pmpro-strong-passwords/pmpro-require-strong-password-minimum-strength.php
@@ -0,0 +1,35 @@
+<?php 
+
+/**
+ * This recipe works with the PMPro Require Strong Passwords Add On to increase 
+ * the minimum password strength required during membership checkout or signup.
+ * Using the pmprosp_minimum_password_score filter 
+ *
+ * title: Require a Stronger Password Score for Member Registration
+ * layout: snippet
+ * collection: add-ons, pmpro-strong-passwords
+ * category: password
+ * link: https://www.paidmembershipspro.com/add-ons/require-strong-passwords/
+ *
+ * You can add this recipe to your site by creating a custom plugin
+ * or using the Code Snippets plugin available for free in the WordPress repository.
+ * Read this companion article for step-by-step directions on either method.
+ * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
+ */
+
+/**
+ * Set the minimum acceptable password score to 3 (strong).
+ * Score Reference:
+ * 0 = very weak
+ * 1 = weak
+ * 2 = medium
+ * 3 = strong
+ * 4 = very strong
+ *
+ */
+function my_pmprosp_minimum_password_score( $min_strength, $password_strength ) {
+	$min_strength = 3; // Require strong or very strong passwords.
+	return $min_strength;
+}
+add_filter( 'pmprosp_minimum_password_score', 'my_pmprosp_minimum_password_score', 10, 2 );
+
