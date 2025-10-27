8580402dbe4b7fe84a28dd1f7f72dfc3ba3a32f1
Kim White <kim@pixelpaper.net>
2025-10-27 13:42:45 -0400

    Hide Discount Code Fields
    These are for this post
    https://www.paidmembershipspro.com/hide-discount-code-field-on-membership-checkout-for-free-levels/
    
    didn't change anything

---
+++ discount-codes/hide_discount_code_field_for_free_levels.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Hide discount code field on the PMPro checkout page for all free levels.
+ * 
+ * title: Hide discount code field on the PMPro checkout page for all free levels.
+ * layout: snippet
+ * collection: discount-codes
+ * category: checkout
+ * link: https://www.paidmembershipspro.com/hide-discount-code-field-on-membership-checkout-for-free-levels/
+ * 
+ * You can add this recipe to your site by creating a custom plugin
+ * or using the Code Snippets plugin available for free in the WordPress repository.
+ * Read this companion article for step-by-step directions on either method.
+ * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
+ */
+function hide_discount_code_field_for_free_levels($show)
+{
+  global $pmpro_level;
+  
+  if(function_exists('pmpro_isLevelFree') && pmpro_isLevelFree($pmpro_level))
+    $show = false;
+
+  return $show;
+}
+add_filter('pmpro_show_discount_code', 'hide_discount_code_field_for_free_levels');
