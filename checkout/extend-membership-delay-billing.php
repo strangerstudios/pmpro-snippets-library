a3377e0e37e41f3af7a18c5f4337aec342bb4d2c
kristysut <64672103+kristysut@users.noreply.github.com>
2025-10-28 12:06:04 -0400

    Update extend-membership-delay-billing.php
    Updated URL

--- checkout/extend-membership-delay-billing.php
+++ checkout/extend-membership-delay-billing.php
@@ -10,7 +10,7 @@
  * layout: snippet
  * collection: checkout
  * category: renewals
- * link: https://www.paidmembershipspro.com/keep-existing-expiration-recurring-billing/
+ * link: https://www.paidmembershipspro.com/keep-existing-expiration-date-renewal/
  *
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
