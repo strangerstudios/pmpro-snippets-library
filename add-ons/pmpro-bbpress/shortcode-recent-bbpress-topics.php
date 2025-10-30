91bf4a5e23e4265528bd34cc9c721c67e3845553
Kim White <kim@pixelpaper.net>
2025-10-30 16:11:00 -0400

    4 Snippets for new post
    "Build Membership Account/Dashboard in WordPress"

---
+++ add-ons/pmpro-bbpress/shortcode-recent-bbpress-topics.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Create a shortcode to display the most recent bbPress topics with linked titles, author name, and date.
+ *
+ * title: Shortcode to Display Recent bbPress Topics
+ * layout: snippet-example
+ * collection: shortcodes
+ * category: bbpress
+ * link: TBD
+ *
+ * You can add this recipe to your site by creating a custom plugin
+ * or using the Code Snippets plugin available for free in the WordPress repository.
+ * Read this companion article for step-by-step directions on either method.
+ * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
+ */
+
+function custom_bbpress_recent_topics($atts) {
+    $atts = shortcode_atts(array(
+        'show' => 3,
+    ), $atts, 'bbpress_recent_topics');
+
+    $args = array(
+        'posts_per_page' => intval($atts['show']),
+        'post_type'      => 'topic',
+        'post_status'    => 'publish',
+        'orderby'        => 'date',
+        'order'          => 'DESC',
+    );
+
+    $topics = new WP_Query($args);
+    ob_start();
+
+    if ($topics->have_posts()) {
+        $count = 1;
+        while ($topics->have_posts()) {
+            $topics->the_post();
+            ?>
+            <div class="bbpress-recent-topic-row row-<?php echo $count % 2 === 0 ? 'even' : 'odd'; ?>">
+                <div><strong><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></strong></div>
+                <small>By: <?php the_author(); ?> on <?php the_time(get_option('date_format')); ?></small>
+            </div>
+            <hr />
+            <?php
+            $count++;
+        }
+        wp_reset_postdata();
+    } else {
+        echo '<p>No recent topics found.</p>';
+    }
+
+    return ob_get_clean();
+}
+add_shortcode('bbpress_recent_topics', 'custom_bbpress_recent_topics');
---
+++ frontend-pages/account-page-action-links-by-level.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Add or remove action links from the Membership Account page based on the member’s level.
+ *
+ * title: Customize Account Page Action Links by Level
+ * layout: snippet-example
+ * collection: frontend-pages
+ * category: account-page
+ * link: TBD
+ *
+ * You can add this recipe to your site by creating a custom plugin
+ * or using the Code Snippets plugin available for free in the WordPress repository.
+ * Read this companion article for step-by-step directions on either method.
+ * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
+ */
+
+function my_member_action_links( $pmpro_member_action_links, $level_id ) {
+	global $current_user;
+	$new_pmpro_member_action_links = array();
+
+	// Always unset the 'change' and 'update-billing' links.
+	unset( $pmpro_member_action_links['change'] );
+
+	// Add link to onboarding page.
+	if ( in_array( $level_id, array( 1 ) ) ) {
+		$new_pmpro_member_action_links['upgrade'] = '<a href="/membership-checkout/?pmpro_level=5">Upgrade</a>';
+	}
+
+	// Add link to get support for Website plan members.
+	if ( in_array( $level_id, array( 5 ) ) ) {
+		$new_pmpro_member_action_links['feedback'] = '<a href="#">Share Feedback</a>';
+	}
+
+	// Remove cancel link for coaching levels.
+	if ( in_array( $level_id, array( 6, 7, 8 ) ) ) {
+		unset( $pmpro_member_action_links['cancel'] );
+	}
+
+	// Merge the new links with the existing links.
+	$pmpro_member_action_links = array_merge( $new_pmpro_member_action_links, $pmpro_member_action_links );
+
+	return $pmpro_member_action_links;
+}
+add_filter( 'pmpro_member_action_links', 'my_member_action_links', 10, 2 );
