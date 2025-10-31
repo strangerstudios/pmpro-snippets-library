ec12fb3e91a0dcef2230fb629cc8a58ccd93811d
David Wanjuki <123358258+dwanjuki@users.noreply.github.com>
2025-10-31 18:33:30 +0300

    Add snippet

---
+++ integration-compatibility/disable-popup-maker-popups-for-members-of-a-level.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Disable all Popup Maker popups if user has membership level 1.
+ *
+ * title: Disable all Popup Maker popups if user has membership level 1.
+ * layout: snippet
+ * collection: integration-compatibility
+ * category: content, popup-maker
+ * link: https://www.paidmembershipspro.com/membership-logic-enable-disable-popups-popup-maker-popups-optinmonster/
+ *
+ * You can add this recipe to your site by creating a custom plugin
+ * or using the Code Snippets plugin available for free in the WordPress repository.
+ * Read this companion article for step-by-step directions on either method.
+ * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
+ */
+function my_pmpro_pum_popup_is_loadable( $is_loadable, $popup_id ) {
+	if ( $is_loadable && function_exists( 'pmpro_hasMembershipLevel' ) && pmpro_hasMembershipLevel( 1 ) ) {
+		$is_loadable = false;
+	}
+	return $is_loadable;
+}
+add_filter( 'pum_popup_is_loadable', 'my_pmpro_pum_popup_is_loadable', 1000, 2 );
