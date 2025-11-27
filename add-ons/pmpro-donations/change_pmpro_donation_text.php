20d978030ed7edc9c84904d3e3e5fcc0f6d6deb8
Andrew Lima <andrew@arctek.co.za>
2025-11-27 08:51:55 +0200

    Update change_pmpro_donation_text.php

--- add-ons/pmpro-donations/change_pmpro_donation_text.php
+++ add-ons/pmpro-donations/change_pmpro_donation_text.php
@@ -1,26 +1,24 @@
 <?php
 /**
- * This function simply edits the text 'Make a Gift' for PMPRO Donations Add-On
+ * Edit the text 'Make a Gift' for PMPro Donations Add On
  * 
- * title: Change "Make a Gift" Text for PMPRO Donations Add-On
+ * title: Change "Make a Gift" Text for PMPro Donations Add On
  * layout: snippet
  * collection: add-ons, pmpro-donations
- * category: users, custom user fields
- * link: https://www.paidmembershipspro.com/add-ons/donations-add-on/
+ * category: text-change, customization
+ * link: TBD
  *
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
  * Read this companion article for step-by-step directions on either method.
  * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
  */
-
-
-function pmpro_donations_change_text( $change_text, $text, $domain ) {
+function my_pmpro_donations_change_text( $change_text, $text, $domain ) {
     switch ( $change_text ) {
         case 'Make a Gift' :
-            $change_text = __( 'Your Replacement Text Here', 'pmpro-donations' ); //edit 'This will change Make A Gift' to edit the text output of 'Make a Gift'
+            $change_text = __( 'Your Replacement Text Here', 'pmpro-donations' ); //Replace the text in the brackets to adjust the "Make a Gift" wording shown.
             break;
     }
     return $change_text;
 }
-add_filter( 'gettext', 'pmpro_donations_change_text', 20, 3 );
+add_filter( 'gettext', 'my_pmpro_donations_change_text', 20, 3 );
