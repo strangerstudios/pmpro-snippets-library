432b3093122612ffbb42cc12ebcdb743ce5764ca
Kim White <kim@pixelpaper.net>
2026-01-21 12:27:52 -0500

    Add Hide Level snippet
    Remove free membership levels from the levels array used by PMPro.
    
    for exsiting post
    https://www.paidmembershipspro.com/hide-free-levels-from-the-membership-levels-page/

---
+++ frontend-pages/hide-free-levels-from-levels-page.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Remove free membership levels from the levels array used by PMPro.
+ *
+ * title: Hide Free Levels in PMPro Levels Page
+ * layout: snippet
+ * collection: membership-levels
+ * category: levels
+ * link: https://www.paidmembershipspro.com/hide-free-levels-from-the-membership-levels-page/
+ *
+ * You can add this recipe to your site by creating a custom plugin
+ * or using the Code Snippets plugin available for free in the WordPress repository.
+ * Read this companion article for step-by-step directions on either method.
+ * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
+ */
+
+function pmpro_snippet_hide_free_levels_from_levels_array( $levels ) {
+	if ( empty( $levels ) || ! is_array( $levels ) ) {
+		return $levels;
+	}
+
+	$levels = array_filter(
+		$levels,
+		static function( $level ) {
+			return ! pmpro_isLevelFree( $level );
+		}
+	);
+
+	// Re-index the array (optional, but keeps output tidy).
+	return array_values( $levels );
+}
+add_filter( 'pmpro_levels_array', 'pmpro_snippet_hide_free_levels_from_levels_array' );
