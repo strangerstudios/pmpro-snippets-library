f31b46b59a99afb4d5362fe39f5b3578b07b4d40
RachieVee <RachelRVasquez@users.noreply.github.com>
2026-03-16 07:38:08 -0400

    Add link to redirect logout article
    Added a link to the redirect logout article for reference.

--- misc/redirect-member-on-logout.php
+++ misc/redirect-member-on-logout.php
@@ -7,6 +7,7 @@
  * layout: snippet
  * collection: misc
  * category: logout, redirect
+ * link: https://www.paidmembershipspro.com/redirect-logout/
  *
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
