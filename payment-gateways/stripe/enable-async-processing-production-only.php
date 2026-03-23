7427980f8c2fa816c3952acb4c1651709d120e28
Kim Coleman <kim@strangerstudios.com>
2026-03-23 15:43:47 -0400

    Adding link to live post

--- payment-gateways/stripe/enable-async-processing-production-only.php
+++ payment-gateways/stripe/enable-async-processing-production-only.php
@@ -7,15 +7,15 @@
  * layout: snippet
  * collection: payment-gateways, stripe
  * category: action-scheduler
- * link: tbd
+ * link: https://www.paidmembershipspro.com/async-processing-stripe-webhooks/
  *
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
  * Read this companion article for step-by-step directions on either method.
  * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
  */
-function mysite_enable_async_stripe_webhooks() {
+function my_pmpro_enable_async_stripe_webhooks() {
     return wp_get_environment_type() === 'production';
 }
 
-add_filter( 'pmpro_stripe_webhook_enable_async_processing', 'mysite_enable_async_stripe_webhooks' );
+add_filter( 'pmpro_stripe_webhook_enable_async_processing', 'my_pmpro_enable_async_stripe_webhooks' );
