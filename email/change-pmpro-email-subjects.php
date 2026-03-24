500f097668d09c834363b6545b66a39675a6efb5
Kim Coleman <kim@strangerstudios.com>
2026-03-24 19:58:45 -0400

    Update email subject for checkout_free template

--- email/change-pmpro-email-subjects.php
+++ email/change-pmpro-email-subjects.php
@@ -14,9 +14,9 @@
 * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
 */
 function my_pmpro_email_subject( $subject, $email ) {		
-	//only checkout emails
-	if ( $email->template == 'checkout_free' )	{
-		$subject = 'Thank you, ' . $email->data["name"] . ', for using ' . get_bloginfo( 'name' );
+	// Only the Checkout - Free email to members of level ID 5.
+	if ( $email->template == 'checkout_free' && (int) $email->data['membership_id'] === 5 ) {
+		$subject = "You're in, " . $email->data['name'] . "! Here's everything you need to know about your new membership to " . get_bloginfo( 'name' );
 	}
 
 	return $subject;
