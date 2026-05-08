85e3051f4411afbd2415cd41bf7c292a0b2bd63e
Kim Coleman <kim@strangerstudios.com>
2026-05-08 13:12:05 -0400

    Update cancellation link in membership levels snippet

--- membership-levels/cancel-specific-membership-levels-immediately.php
+++ membership-levels/cancel-specific-membership-levels-immediately.php
@@ -6,7 +6,7 @@
  * layout: snippet
  * collection: membership-levels
  * category: cancellation
- * link: https://www.paidmembershipspro.com/documentation/membership-levels/canceling-a-user-membership/
+ * link: https://www.paidmembershipspro.com/immediate-cancellations/
  *
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
