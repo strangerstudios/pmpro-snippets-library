194a7982c3b8a8fd2d3f938f65e4cf05ae907c71
David Wanjuki <123358258+dwanjuki@users.noreply.github.com>
2026-05-25 11:58:55 +0300

    Track grace periods per level using a separate meta key for each level

--- membership-levels/add-grace-to-membership-level.php
+++ membership-levels/add-grace-to-membership-level.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Add a 15-Day Grace Period to a Membership Level with PMPro
+ * Add a 15-Day Grace Period to Membership Levels with PMPro
  * 
  * title: Add a Grace Period to a Membership Level
  * layout: snippet
  * collection: membership-levels
  * category: code-snippet, membership-level
- * link: https://www.paidmembershipspro.com/add-a-grace-period-to-a-pmpro-membership/
+ * link: https://www.paidmembershipspro.com/add-a-grace-period/
  *
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
@@ -14,17 +14,20 @@
  * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
  */
 function my_pmpro_membership_post_membership_expiry( $user_id, $level_id ) {
-	// Make sure we aren't already in a grace period for this level
-	$grace_level = get_user_meta( $user_id, 'grace_level', true );
-	if ( empty( $grace_level ) || $grace_level !== $level_id ) {
+	// Make sure we aren't already in a grace period for this level.
+	// Use a per-level meta key so members with multiple levels don't overwrite each other's grace period tracking.
+	$grace_period_meta_key = 'pmpro_grace_period_level_' . $level_id;
+	$grace_level_flag      = get_user_meta( $user_id, $grace_period_meta_key, true );
+
+	if ( empty( $grace_level_flag ) ) {
 		$grace_level                  = array();
-		$grace_level['user_id'] = $user_id;
+		$grace_level['user_id']       = $user_id;
 		$grace_level['membership_id'] = $level_id;
 		$grace_level['enddate']       = date( 'Y-m-d H:i:s', strtotime( '+15 days', current_time( 'timestamp' ) ) ); // change +15 days with the number of days you would like to give for the grace period.
 		$changed = pmpro_changeMembershipLevel( $grace_level, $user_id );
-		update_user_meta( $user_id, 'grace_level', $level_id );
+		update_user_meta( $user_id, $grace_period_meta_key, 1 );
 	} else {
-		delete_user_meta( $user_id, 'grace_level' );
+		delete_user_meta( $user_id, $grace_period_meta_key );
 	}
 }
 add_action( 'pmpro_membership_post_membership_expiry', 'my_pmpro_membership_post_membership_expiry', 10, 2 );
