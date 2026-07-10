5fb5ae81dfd10d9ca730f57cfa4d967c5ef2a2cf
RachieVee <RachelRVasquez@users.noreply.github.com>
2026-07-10 10:09:19 -0400

    Add the link to the code recipe post "Change the Admin Email Recipient for PMPro Admin Emails"

--- email/change-who-receives-pmpro-admin-emails.php
+++ email/change-who-receives-pmpro-admin-emails.php
@@ -6,7 +6,7 @@
  * layout: snippet
  * collection: email
  * category: admin
- * link: TBD
+ * link: https://www.paidmembershipspro.com/change-pmpro-admin-email-recipient/
  *
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
