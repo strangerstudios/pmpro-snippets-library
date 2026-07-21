9ebb4bfc9adfb476d05d89e1ca9a50950c200354
Rachel Vasquez <rachelrvasquez@gmail.com>
2026-07-21 14:34:50 -0400

    swap snippet for Flint's suggested fix, tested locally with non-existent user field, user field with weird characters and an empty string - all fail silently without causing any errors or warnings in the debug log. Works on the front-end.

--- add-ons/pmpro-member-directory/sort-pmpro-member-directory-order-by-custom-user-meta-field.php
+++ add-ons/pmpro-member-directory/sort-pmpro-member-directory-order-by-custom-user-meta-field.php
@@ -15,15 +15,12 @@
  */
 function pmpro_sort_member_directory_by_user_field( $parts ) {
 	global $wpdb;
-    
-	$user_meta_key_name = 'company'; // Custom field name to sort by, use the stored database value.
-	$order = 'ASC'; //Change ASC to DESC if you need to change the order displayed based on meta value.
 
-	$parts['SELECT'] = "SELECT SQL_CALC_FOUND_ROWS u.ID, u.user_login, u.user_email, u.user_nicename, u.display_name, UNIX_TIMESTAMP(u.user_registered) as joindate, mu.membership_id, mu.initial_payment, mu.billing_amount, mu.cycle_period, mu.cycle_number, mu.billing_limit, mu.trial_amount, mu.trial_limit, UNIX_TIMESTAMP(mu.startdate) as startdate, UNIX_TIMESTAMP(mu.enddate) as enddate, m.name as membership, umf.meta_value as first_name, uml.meta_value as last_name, umk.meta_value as user_meta_key_name FROM $wpdb->users u ";
+	$user_meta_key_name = 'company';
+	$order = 'ASC';
 
-	$parts['JOIN'] .= " LEFT JOIN $wpdb->usermeta umk ON umk.meta_key = '". esc_sql( $user_meta_key_name ) . "' AND u.ID = umk.user_id ";
-
-	$parts['ORDER'] = ' ORDER BY user_meta_key_name ' . esc_sql( $order ) . ' ';
+	$parts['JOIN']  .= " LEFT JOIN $wpdb->usermeta umk ON umk.meta_key = '" . esc_sql( $user_meta_key_name ) . "' AND u.ID = umk.user_id ";
+	$parts['ORDER']  = " ORDER BY umk.meta_value " . esc_sql( $order ) . " ";
 
 	return $parts;
 }
