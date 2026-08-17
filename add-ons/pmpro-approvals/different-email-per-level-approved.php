bc21e36b2f7dcac988e7987884f77c1cac52967f
Kim White <kim@pixelpaper.net>
2026-08-17 17:04:32 -0400

    Add link to Recipe Post
    added link https://www.paidmembershipspro.com/approval-emails-by-membership-level/#customize-recipe

--- add-ons/pmpro-approvals/different-email-per-level-approved.php
+++ add-ons/pmpro-approvals/different-email-per-level-approved.php
@@ -9,6 +9,7 @@
  * layout: snippet
  * collection: add-ons
  * category: pmpro-approvals
+ * link: https://www.paidmembershipspro.com/approval-emails-by-membership-level/#customize-recipe
  *
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
