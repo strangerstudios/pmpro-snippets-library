5b9735042cac64595e3e94d4d7cff0bcde6af3b8
Kim Coleman <kim@strangerstudios.com>
2026-08-31 16:28:44 -0400

    Update migrate-profilepress-avatars-to-pmpro-avatars.php

--- misc/migrate-profilepress-avatars-to-pmpro-avatars.php
+++ misc/migrate-profilepress-avatars-to-pmpro-avatars.php
@@ -7,7 +7,7 @@
  * layout: snippet
  * collection: misc
  * category: migration
- * link: https://www.paidmembershipspro.com/link-to-post-if-available/ OR TBD
+ * link: https://www.paidmembershipspro.com/migrate-profilepress-avatars/
  * 
  * You can add this recipe to your site by creating a custom plugin
  * or using the Code Snippets plugin available for free in the WordPress repository.
