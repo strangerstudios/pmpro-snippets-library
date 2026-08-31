a634727e04e05cf2a38ad836a337701dec857b65
Andrew Lima <andrew@arctek.co.za>
2026-08-31 16:06:28 +0200

    Simplify the file checker.

--- restricting-content/display-restricted-pdfs-inline-in-browser.php
+++ restricting-content/display-restricted-pdfs-inline-in-browser.php
@@ -2,10 +2,7 @@
 /**
  * Display restricted PDFs inline in the browser instead of forcing a download.
  *
- * By default, Paid Memberships Pro serves restricted images inline and all other
- * file types as downloads. This recipe also serves PDFs inline.
- *
- * Restrict files: https://www.paidmembershipspro.com/locking-down-protecting-files-with-pmpro/
+ * For more information about restricting files with PMPro visit: https://www.paidmembershipspro.com/locking-down-protecting-files-with-pmpro/
  *
  * title: Display Restricted PDFs in the Browser
  * layout: snippet
@@ -19,11 +16,9 @@
  * https://www.paidmembershipspro.com/create-a-plugin-for-pmpro-customizations/
  */
 function my_pmpro_inline_restricted_pdfs( $content_disposition, $file, $file_dir, $file_path ) {
-	$finfo     = finfo_open( FILEINFO_MIME_TYPE );
-	$mime_type = finfo_file( $finfo, $file_path );
-	finfo_close( $finfo );
+	$filetype = wp_check_filetype( $file_path );
 
-	if ( 'application/pdf' === $mime_type ) {
+	if ( 'application/pdf' === $filetype['type'] ) {
 		return 'inline';
 	}
 
