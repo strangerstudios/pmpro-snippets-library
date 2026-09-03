32352f156245f0d479523834e577af07ee28c21d
Kim Coleman <kim@strangerstudios.com>
2026-09-03 07:53:27 -0400

    Refactor sample report registration and rendering
    Updated the registration and rendering functions for a sample report in the Paid Memberships Pro plugin.

--- admin-pages/create-custom-report.php
+++ admin-pages/create-custom-report.php
@@ -1,4 +1,3 @@
-
 <?php
 /**
  * Create a custom report
@@ -17,37 +16,45 @@
  */
 
 /**
- * Add a Custom Report to the Memberships > Reports Screen in Paid Memberships Pro.
+ * Register a custom report on the Memberships > Reports screen.
  *
- * For each report, add a line like:
- * global $pmpro_reports;
- * $pmpro_reports['slug'] = 'Title';
+ * Each report needs a slug and title here, plus two functions named after the slug:
+ * pmpro_report_{slug}_widget() renders the summary box on the Reports dashboard.
+ * pmpro_report_{slug}_page() renders the detail view when someone clicks "Details".
  *
- * For each report, also write two functions:
- * pmpro_report_{slug}_widget()   to show up on the report homepage.
- * pmpro_report_{slug}_page()     to show up when users click on the report page widget.
- *
+ * @param array $pmpro_reports Registered reports, keyed by slug.
+ * @return array
  */
+function my_pmpro_register_sample_report( $pmpro_reports ) {
+	$pmpro_reports['sample'] = __( 'My Sample Report', 'pmpro-snippets-library' );
+	return $pmpro_reports;
+}
+add_filter( 'pmpro_registered_reports', 'my_pmpro_register_sample_report' );
 
- global $pmpro_reports;
- $pmpro_reports['sample'] = __('My Sample Report', 'pmpro-snippets-library');
- 
- // Sample Report for Metabox
- function pmpro_report_sample_widget() {	 ?>
-     <span id="pmpro_report_sample" class="pmpro_report-holder">
-         <p><?php esc_html_e( "Hi! I'm a sample report!", 'pmpro-snippets-library' ); ?></p>
-         <?php if ( function_exists( 'pmpro_report_sample_page' ) ) { ?>
-             <p class="pmpro_report-button">
-                 <a class="button button-primary" href="<?php echo admin_url( 'admin.php?page=pmpro-reports&report=sample' ); ?>"><?php _e( 'Details', 'pmpro-snippets-library' );?></a>
-             </p>
-         <?php } ?>	
-     </span>
-     <?php
- }
- 
- // Sample Report for Individual Report Page
- function pmpro_report_sample_page() { ?>
-     <h1><?php _e( 'This is a Sample', 'pmpro-snippets-library' ); ?></h2>
-     <p><?php esc_html_e( 'This report demonstrates how to add a custom report to PMPro. Enjoy!.', 'pmpro-snippets-library' ); ?></p>
-     <?php
- }
+/**
+ * Render the summary widget on the Reports dashboard.
+ *
+ * @return void
+ */
+function pmpro_report_sample_widget() { ?>
+	<span id="pmpro_report_sample" class="pmpro_report-holder">
+		<p><?php esc_html_e( 'Hi! I am a sample report.', 'pmpro-snippets-library' ); ?></p>
+		<?php if ( function_exists( 'pmpro_report_sample_page' ) ) { ?>
+			<p class="pmpro_report-button">
+				<a class="button button-primary" href="<?php echo esc_url( admin_url( 'admin.php?page=pmpro-reports&report=sample' ) ); ?>"><?php esc_html_e( 'Details', 'pmpro-snippets-library' ); ?></a>
+			</p>
+		<?php } ?>
+	</span>
+	<?php
+}
+
+/**
+ * Render the detail view for the report.
+ *
+ * @return void
+ */
+function pmpro_report_sample_page() { ?>
+	<h1 class="wp-heading-inline"><?php esc_html_e( 'This is a Sample', 'pmpro-snippets-library' ); ?></h1>
+	<p><?php esc_html_e( 'This report demonstrates how to add a custom report to PMPro.', 'pmpro-snippets-library' ); ?></p>
+	<?php
+}
